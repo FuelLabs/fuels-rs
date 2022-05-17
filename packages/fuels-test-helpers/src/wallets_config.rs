be21ad357580a6adc1c8d1476c55b210ef60a75e
MujkicA <ahmed.mujkic@fuel.sh>
2022-05-17 17:43:35 +0200

    Add wallets config

---
+++ packages/fuels-test-helpers/src/wallets_config.rs
@@ -0,0 +1,34 @@
+pub const DEFAULT_COIN_AMOUNT: u64 = 1_000_000_000;
+pub const DEFAULT_NUM_COINS: u64 = 1;
+pub const DEFAULT_NUM_WALLETS: u64 = 10;
+
+#[derive(Debug)]
+pub struct WalletsConfig {
+    pub num_wallets: u64,
+    pub coins_per_wallet: u64,
+    pub coin_amount: u64,
+}
+
+impl WalletsConfig {
+    pub fn new(
+        num_wallets: Option<u64>,
+        coins_per_wallet: Option<u64>,
+        amount_per_coin: Option<u64>,
+    ) -> Self {
+        Self {
+            num_wallets: num_wallets.unwrap_or(DEFAULT_NUM_WALLETS),
+            coins_per_wallet: amount_per_coin.unwrap_or(DEFAULT_NUM_COINS),
+            coin_amount: coins_per_wallet.unwrap_or(DEFAULT_COIN_AMOUNT),
+        }
+    }
+}
+
+impl Default for WalletsConfig {
+    fn default() -> Self {
+        Self {
+            num_wallets: DEFAULT_NUM_WALLETS,
+            coins_per_wallet: DEFAULT_NUM_COINS,
+            coin_amount: DEFAULT_COIN_AMOUNT,
+        }
+    }
+}
