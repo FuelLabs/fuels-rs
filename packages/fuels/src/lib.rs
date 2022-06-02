f23523df5a8aeca25f7b51fc5bede2528c9e61aa
Oleksii Filonenko <brightone@protonmail.com>
2022-06-02 22:12:20 +0300

    docs(examples): add instantiate_client example (#291)
    - re-export `fuel_core` and `fuel_gql_client` from `fuels`
    - create `instantiate_client` test in `examples/contracts` (could be moved later)
    - include `instantiate_client` code in `getting-started/basics.md`

--- packages/fuels/src/lib.rs
+++ packages/fuels/src/lib.rs
@@ -16,12 +16,21 @@
 //! Examples on how you can use the types imported by the prelude can be found in
 //! the [main test suite](https://github.com/FuelLabs/fuels-rs/blob/master/fuels-abigen-macro/tests/harness.rs)
 
+pub mod client {
+    pub use fuel_gql_client::client::*;
+}
+
 pub mod contract {
     pub use fuels_contract::*;
 }
 
 pub mod core {
     pub use fuels_core::*;
+}
+
+pub mod node {
+    #[doc(no_inline)]
+    pub use fuel_core::*;
 }
 
 pub mod signers {
