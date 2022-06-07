91fd5d37880864bbcc072d5e35596139b91adebe
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2022-06-07 20:11:48 +0200

    improve readability of tests

--- packages/fuels-core/src/encoding_utils.rs
+++ packages/fuels-core/src/encoding_utils.rs
@@ -1,17 +1,11 @@
 use crate::{ParamType, WORD_SIZE};
 
 /// If given a non-empty slice it will return the number of `WORD`s needed to fit the biggest of the provided types.
-///
-///
-/// returns: Option<usize>
-///
 pub fn max_by_encoding_width(params: &[ParamType]) -> Option<usize> {
     params.iter().map(encoding_width).max()
 }
 
 /// Calculates the number of `WORD`s the VM expects this parameter to be encoded in.
-/// # Panics
-/// Calculating will panic if you pass an ParamType::Enum without any variants since that is an invalid type.
 pub fn encoding_width(param: &ParamType) -> usize {
     const fn count_words(bytes: usize) -> usize {
         let q = bytes / WORD_SIZE;
@@ -44,35 +38,45 @@ pub fn encoding_width(param: &ParamType) -> usize {
 
 #[cfg(test)]
 mod tests {
+    const WIDTH_OF_B256: usize = 4;
+    const WIDTH_OF_U32: usize = 1;
+    const WIDTH_OF_BOOL: usize = 1;
     use crate::encoding_utils::encoding_width;
     use crate::{EnumVariants, ParamType};
 
     #[test]
     fn array_size_dependent_on_num_of_elements() {
-        let param = ParamType::Array(Box::new(ParamType::B256), 11);
+        const NUM_ELEMENTS: usize = 11;
+        let param = ParamType::Array(Box::new(ParamType::B256), NUM_ELEMENTS);
 
         let width = encoding_width(&param);
 
-        assert_eq!(44, width);
+        let expected = NUM_ELEMENTS * WIDTH_OF_B256;
+        assert_eq!(expected, width);
     }
 
     #[test]
     fn string_size_dependent_on_num_of_elements() {
-        let param = ParamType::String(9);
+        const NUM_ASCII_CHARS: usize = 9;
+        let param = ParamType::String(NUM_ASCII_CHARS);
 
         let width = encoding_width(&param);
 
+        // 2 WORDS or 16 B are enough to fit 9 ascii chars
         assert_eq!(2, width);
     }
 
     #[test]
     fn structs_are_just_all_elements_combined() {
         let inner_struct = ParamType::Struct(vec![ParamType::U32, ParamType::U32]);
-        let param = ParamType::Struct(vec![ParamType::B256, ParamType::Bool, inner_struct]);
 
-        let width = encoding_width(&param);
+        let a_struct = ParamType::Struct(vec![ParamType::B256, ParamType::Bool, inner_struct]);
 
-        assert_eq!(7, width);
+        let width = encoding_width(&a_struct);
+
+        const INNER_STRUCT_WIDTH: usize = WIDTH_OF_U32 * 2;
+        const EXPECTED_WIDTH: usize = WIDTH_OF_B256 + WIDTH_OF_BOOL + INNER_STRUCT_WIDTH;
+        assert_eq!(EXPECTED_WIDTH, width);
     }
 
     #[test]
@@ -82,7 +86,9 @@ mod tests {
 
         let width = encoding_width(&param);
 
-        assert_eq!(5, width);
+        const INNER_STRUCT_SIZE: usize = WIDTH_OF_B256;
+        const EXPECTED_WIDTH: usize = INNER_STRUCT_SIZE + 1;
+        assert_eq!(EXPECTED_WIDTH, width);
     }
 
     #[test]
@@ -92,6 +98,8 @@ mod tests {
 
         let width = encoding_width(&param);
 
-        assert_eq!(5, width);
+        const INNER_TUPLE_WIDTH: usize = WIDTH_OF_B256;
+        const EXPECTED_WIDTH: usize = WIDTH_OF_U32 + INNER_TUPLE_WIDTH;
+        assert_eq!(EXPECTED_WIDTH, width);
     }
 }
