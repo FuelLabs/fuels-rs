c69ec97e0b1f05b385e8dfb1c385143e4b68b9bb
Rodrigo Araújo <rod.dearaujo@gmail.com>
2022-06-08 14:35:36 -0700

    ci: update forc to 0.15.1 (#374)

--- scripts/build-test-projects/src/main.rs
+++ scripts/build-test-projects/src/main.rs
@@ -10,6 +10,15 @@ use std::{
 };
 
 fn main() {
+    let output = std::process::Command::new("forc")
+        .args(["--version"])
+        .output()
+        .expect("failed to run `forc --version`");
+
+    let version = String::from_utf8(output.stdout).expect("failed to parse forc --version output");
+
+    println!("Building projects with: {:?}", version);
+
     let path = Path::new("packages/fuels-abigen-macro/tests/test_projects");
     let cwd = env::current_dir().unwrap();
     let final_path = cwd.join(path);
