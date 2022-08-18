ba59b175183b5c212224cf721ce07a1f736a589c
Emir Salkic <emirsalkicart@gmail.com>
2022-08-18 21:30:35 +0200

    check anchors

--- scripts/check-docs/src/main.rs
+++ scripts/check-docs/src/main.rs
@@ -2,7 +2,7 @@ use std::process::Stdio;
 
 fn main() {
 
-    let rg = std::process::Command::new("rg")
+    let rg_project = std::process::Command::new("rg")
         .arg("--case-sensitive")
         .arg("-g")
         .arg("!/scripts/check-docs/src/main.rs")
@@ -11,18 +11,46 @@ fn main() {
         .spawn()
         .expect("failed rg command");
 
-    let output = std::process::Command::new("sed")
+    let output_files = std::process::Command::new("sed")
         .arg("s/ //g; s=//==g")
-        .stdin(rg.stdout.unwrap()) // Converted into a Stdio here
+        .stdin(rg_project.stdout.unwrap())
         .output()
         .expect("failed sed command");
 
-    println!("\n \n");
+    let mut stack = vec![];
 
-    let output_to_string = String::from_utf8(output.stdout).expect("failed to parse command output");
+    let output_to_string = String::from_utf8(output_files.stdout).expect("failed to parse command output");
+    let split = output_to_string.split("\n");
+    let vec_of_anchors = split.into_iter().filter(|s| !s.is_empty()).collect::<Vec<&str>>();
 
-    let mut split = output_to_string.split("\n");
-    let vec = split.collect::<Vec<&str>>();
+    for i in vec_of_anchors {
+        let mut parts = i.split(':').collect::<Vec<&str>>();
+        if parts.get(1).expect("error in parsing vec_of_anchors").to_string() == "ANCHOR" {
+            stack.push(parts.get(2).unwrap().clone());
+        } else {
+            if stack.pop().unwrap() != parts.get(2).unwrap().clone() {
+                panic!("ANCHOR_END of \"{}\" cannot be found", parts.get(2).unwrap().clone());
+            }
+        }
+    }
 
+    let rg_docs = std::process::Command::new("rg")
+        .arg("--case-sensitive")
+        .arg("--no-filename")
+        .arg("\\{\\{#include")
+        .arg("fuels-rs/docs")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed rg command");
+    //
+    // let output_docs = std::process::Command::new("sed")
+    //     .arg("s={{#include==g; s=}}==g")
+    //     .stdin(rg_project.stdout.unwrap())
+    //     .output()
+    //     .expect("failed sed command");
+
+    // let output_docs_to_string = String::from_utf8(output_docs.stdout).expect("failed to parse command output");
+    //
+    // println!("{:?}", output_docs)
 
 }
