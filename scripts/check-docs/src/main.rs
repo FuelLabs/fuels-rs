6fde9ba57d3ae61e626a8af2de6e37495048a652
Emir Salkic <emirsalkicart@gmail.com>
2022-08-19 13:27:00 +0200

    add check validity of anchors

--- scripts/check-docs/src/main.rs
+++ scripts/check-docs/src/main.rs
@@ -1,22 +1,29 @@
+extern crate core;
+
+use std::collections::HashSet;
+use std::env;
+use std::path::Path;
 use std::process::Stdio;
 
 fn main() {
+    let mut stack = vec![];
+    let mut valid_anchors = HashSet::new();
+
     let rg_project = std::process::Command::new("rg")
-        .arg("--case-sensitive")
-        .arg("-g")
-        .arg("!/scripts/check-docs/src/main.rs")
-        .arg("ANCHOR")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed rg command");
+    .arg("--case-sensitive")
+    .arg("-g")
+    .arg("!/scripts/check-docs/src/main.rs")
+    .arg("ANCHOR")
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect("failed rg command");
 
     let output_files = std::process::Command::new("sed")
-        .arg("s/ //g; s=//==g")
-        .stdin(rg_project.stdout.unwrap())
-        .output()
-        .expect("failed sed command");
+    .arg("s/ //g; s=//==g")
+    .stdin(rg_project.stdout.unwrap())
+    .output()
+    .expect("failed sed command");
 
-    let mut stack = vec![];
 
     let output_to_string =
         String::from_utf8(output_files.stdout).expect("failed to parse command output");
@@ -31,7 +38,9 @@ fn main() {
         if *parts.get(1).expect("error in parsing vec_of_anchors") == "ANCHOR" {
             stack.push(*parts.get(2).expect("error"));
             // println!("{:?}", stack);
-        } else {
+        }
+        else if *parts.get(1).expect("error in parsing vec_of_anchors") == "ANCHOR_END" {
+            // TODO test this
             if stack.is_empty() {
                 panic!(
                     "ANCHOR of \"{}\" cannot be found",
@@ -41,21 +50,26 @@ fn main() {
 
             if stack.pop().unwrap() != *parts.get(2).expect("error") {
                 panic!(
-                    "ANCHOR_END of \"{}\" cannot be found",
+                    "ANCHOR or ANCHOR_END of \"{}\" is wrong",
                     *parts.get(2).expect("error")
                 );
             }
             // println!("{:?}", stack);
+        } else {
+            panic!(
+                "Invalid syntax for ANCHOR or ANCHOR_END\"{}\"",
+                *parts.get(1).expect("error")
+            );
         }
-    }
 
-    // println!("vani : {:?}", stack);
+        valid_anchors.insert(*parts.get(2).expect("error in parsing vec_of_anchors"));
+
+    }
 
     let rg_docs = std::process::Command::new("rg")
         .arg("--case-sensitive")
         .arg("-g")
         .arg("!/scripts/check-docs/src/main.rs")
-        .arg("--no-filename")
         .arg("\\{\\{#include")
         .stdout(Stdio::piped())
         .spawn()
@@ -76,7 +90,23 @@ fn main() {
         .collect::<Vec<&str>>();
 
     for i in vec_of_doc_anchors {
-        println!("{:?}", i);
+        let parts = i.split(':').collect::<Vec<&str>>();
+
+        let mut root = (*parts.get(0).unwrap()).rsplitn(2,"/");
+        let _file_name = root.next().unwrap();
+        let _folder_path = root.next().unwrap();
+
+        let _ = env::set_current_dir(&_folder_path).is_ok();
+
+        if !Path::new(*parts.get(1).unwrap()).exists() {
+           panic!("Cannot find path to \"{}\"", *parts.get(1).expect("error"));
+        }
+
+        if parts.get(2).is_some() && !valid_anchors.contains(*parts.get(2).unwrap()){
+            panic!("Cannot find anchor \"{}\" on path \"{}\"", *parts.get(2).unwrap(), *parts.get(1).unwrap());
+        }
+
+
     }
 
 }
