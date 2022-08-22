414fd0af70dde434aa7b0609710ea541a8f42186
Emir Salkic <emirsalkicart@gmail.com>
2022-08-22 09:03:57 +0200

    add refactoring

--- scripts/check-docs/src/main.rs
+++ scripts/check-docs/src/main.rs
@@ -3,41 +3,38 @@ extern crate core;
 use std::collections::{HashMap, HashSet};
 use std::env;
 use std::path::Path;
-use std::process::Stdio;
 
 fn main() {
-    let mut stack = vec![];
+    let mut stack: Vec<&str> = vec![];
     let mut valid_anchors: HashMap<&str, HashSet<&str>> = HashMap::new();
 
-    let rg_project = std::process::Command::new("rg")
-        .arg("--case-sensitive")
-        .arg("-g")
-        .arg("!/scripts/check-docs/src/main.rs")
+    let rg_project = std::process::Command::new("grep")
+        .arg("-I")
+        .arg("-H")
+        .arg("-R")
+        .arg("--exclude-dir=scripts")
         .arg("ANCHOR")
-        .stdout(Stdio::piped())
-        .spawn()
+        .arg(".")
+        .output()
         .expect("failed rg command");
 
-    let output_files = std::process::Command::new("sed")
-        .arg("s/ //g; s=//==g")
-        .stdin(rg_project.stdout.unwrap())
-        .output()
-        .expect("failed sed command");
-
     let output_to_string =
-        String::from_utf8(output_files.stdout).expect("failed to parse command output");
+        String::from_utf8(rg_project.stdout).expect("failed to parse command output");
+
     let split = output_to_string.split('\n');
+
     let vec_of_anchors = split
         .into_iter()
         .filter(|s| !s.is_empty())
-        .collect::<Vec<&str>>();
+        .map(|g| g.replace("./", "").replace(' ', "").replace("//", ""))
+        .collect::<Vec<_>>();
 
-    for i in vec_of_anchors {
-        let parts = i.split(':').collect::<Vec<&str>>();
+    for i in &vec_of_anchors {
+        let parts = i.split(':').collect::<Vec<_>>();
+
         if *parts.get(1).expect("error in parsing vec_of_anchors") == "ANCHOR" {
             stack.push(*parts.get(2).expect("error"));
         } else if *parts.get(1).expect("error in parsing vec_of_anchors") == "ANCHOR_END" {
-            // TODO test this
             if stack.is_empty() {
                 panic!(
                     "ANCHOR of \"{}\" cannot be found",
@@ -71,32 +68,31 @@ fn main() {
         }
     }
 
-    let rg_docs = std::process::Command::new("rg")
-        .arg("--case-sensitive")
-        .arg("-g")
-        .arg("!/scripts/check-docs/src/main.rs")
-        .arg("\\{\\{#include")
-        .stdout(Stdio::piped())
-        .spawn()
+    let rg_docs = std::process::Command::new("grep")
+        .arg("-I")
+        .arg("-H")
+        .arg("-R")
+        .arg("--exclude-dir=scripts")
+        .arg("{{#include")
+        .output()
         .expect("failed rg command");
 
-    let output_docs = std::process::Command::new("sed")
-        .arg("s/ //g; s={{#include==g; s=}}==g")
-        .stdin(rg_docs.stdout.unwrap())
-        .output()
-        .expect("failed sed command");
-
     let output_docs_to_string =
-        String::from_utf8(output_docs.stdout).expect("failed to parse command output");
+        String::from_utf8(rg_docs.stdout).expect("failed to parse command output");
 
     let split = output_docs_to_string.split('\n');
     let vec_of_doc_anchors = split
         .into_iter()
         .filter(|s| !s.is_empty())
-        .collect::<Vec<&str>>();
+        .map(|g| {
+            g.replace(' ', "")
+                .replace("{{#include", "")
+                .replace("}}", "")
+        })
+        .collect::<Vec<_>>();
 
     for i in vec_of_doc_anchors {
-        let parts = i.split(':').collect::<Vec<&str>>();
+        let parts = i.split(':').collect::<Vec<_>>();
 
         let (_folder_path, _file_name) = (*parts.get(0).unwrap()).rsplit_once('/').unwrap();
 
