6600f1121b2ae09fadfd460637d3fd39cf687b29
mitchmindtree <mitchell.nordine@fuel.sh>
2022-08-27 12:23:28 +1000

    Update new flat_abigen module for LocalWallet -> WalletUnlocked

--- packages/fuels-core/src/code_gen/flat_abigen.rs
+++ packages/fuels-core/src/code_gen/flat_abigen.rs
@@ -99,7 +99,7 @@ impl FlatAbigen {
                 quote! {
                     use fuels::contract::contract::{Contract, ContractCallHandler};
                     use fuels::core::{EnumSelector, StringToken, Parameterize, Tokenizable, Token, try_from_bytes};
-                    use fuels::signers::LocalWallet;
+                    use fuels::signers::WalletUnlocked;
                     use fuels::tx::{ContractId, Address};
                     use fuels::types::bech32::Bech32ContractId;
                     use fuels::types::errors::Error as SDKError;
@@ -109,7 +109,7 @@ impl FlatAbigen {
                 quote! {
                     pub struct #name {
                         contract_id: Bech32ContractId,
-                        wallet: LocalWallet
+                        wallet: WalletUnlocked
                     }
 
                     impl #name {
@@ -119,11 +119,11 @@ impl FlatAbigen {
                             &self.contract_id
                         }
 
-                        pub fn _get_wallet(&self) -> LocalWallet {
+                        pub fn _get_wallet(&self) -> WalletUnlocked {
                             self.wallet.clone()
                         }
 
-                        pub fn _with_wallet(&self, mut wallet: LocalWallet) -> Result<Self, SDKError> {
+                        pub fn _with_wallet(&self, mut wallet: WalletUnlocked) -> Result<Self, SDKError> {
                            let provider = self.wallet.get_provider()?;
                            wallet.set_provider(provider.clone());
 
@@ -133,11 +133,11 @@ impl FlatAbigen {
 
                     pub struct #builder_name {
                         contract_id: Bech32ContractId,
-                        wallet: LocalWallet
+                        wallet: WalletUnlocked
                     }
 
                     impl #builder_name {
-                        pub fn new(contract_id: String, wallet: LocalWallet) -> Self {
+                        pub fn new(contract_id: String, wallet: WalletUnlocked) -> Self {
                             let contract_id = Bech32ContractId::from_str(&contract_id).expect("Invalid contract id");
                             Self { contract_id, wallet }
                         }
@@ -147,7 +147,7 @@ impl FlatAbigen {
                             self
                         }
 
-                        pub fn wallet(&mut self, wallet: LocalWallet) -> &mut Self {
+                        pub fn wallet(&mut self, wallet: WalletUnlocked) -> &mut Self {
                             self.wallet = wallet;
                             self
                         }
