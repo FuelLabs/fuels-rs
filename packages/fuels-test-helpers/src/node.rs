12d27bb62bf725af8011647cf066031a1d490515
Emir Salkic <emirsalkicart@gmail.com>
2022-09-01 00:18:41 +0200

    add Messages for .bin

--- packages/fuels-test-helpers/src/node.rs
+++ packages/fuels-test-helpers/src/node.rs
@@ -47,6 +47,42 @@ impl Config {
 
 #[skip_serializing_none]
 #[serde_as]
+#[derive(Clone, Debug, Default, Deserialize, Serialize, Eq, PartialEq)]
+pub struct MessageConfig {
+    #[serde_as(as = "HexType")]
+    pub sender: Address,
+    #[serde_as(as = "HexType")]
+    pub recipient: Address,
+    #[serde_as(as = "HexType")]
+    pub owner: Address,
+    #[serde_as(as = "HexNumber")]
+    pub nonce: Word,
+    #[serde_as(as = "HexNumber")]
+    pub amount: Word,
+    #[serde_as(as = "HexType")]
+    pub data: Vec<u8>,
+    /// The block height from the parent da layer that originated this message
+    #[serde_as(as = "HexNumber")]
+    pub da_height: DaBlockHeight,
+}
+
+impl From<MessageConfig> for Message {
+    fn from(msg: MessageConfig) -> Self {
+        Message {
+            sender: msg.sender,
+            recipient: msg.recipient,
+            owner: msg.owner,
+            nonce: msg.nonce,
+            amount: msg.amount,
+            data: msg.data,
+            da_height: msg.da_height,
+            fuel_block_spend: None,
+        }
+    }
+}
+
+#[skip_serializing_none]
+#[serde_as]
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct CoinConfig {
     #[serde_as(as = "Option<HexType>")]
