0907f9714c1762a00dbc64b2eaaf1f1ae244b3ed
Emir Salkic <emirsalkicart@gmail.com>
2022-10-04 18:08:26 +0200

    add Option<Provider> as param.

--- packages/fuels-test-helpers/src/script.rs
+++ packages/fuels-test-helpers/src/script.rs
@@ -13,30 +13,14 @@ use fuels_types::errors::Error;
 
 /// Run the script binary located at `binary_filepath` and return its resulting receipts,
 /// without having to setup a node or contract bindings.
-#[allow(dead_code)]
-#[cfg(feature = "fuel-core-lib")]
 pub async fn run_compiled_script(
     binary_filepath: &str,
     tx_params: TxParameters,
+    provider: Option<Provider>,
 ) -> Result<Vec<Receipt>, Error> {
     let script_binary = std::fs::read(binary_filepath)?;
     let server = FuelService::new_node(Config::local_node()).await.unwrap();
-    let provider = Provider::connect(server.bound_address.to_string()).await?;
-
-    let script = build_script(script_binary, tx_params);
-
-    script.call(&provider).await
-}
-
-#[allow(dead_code)]
-#[cfg(not(feature = "fuel-core-lib"))]
-pub async fn run_compiled_script(
-    binary_filepath: &str,
-    tx_params: TxParameters,
-) -> Result<Vec<Receipt>, Error> {
-    let script_binary = std::fs::read(binary_filepath)?;
-    let server = FuelService::new_node(Config::local_node()).await.unwrap();
-    let provider = Provider::connect(server.bound_address.to_string()).await?;
+    let provider = provider.unwrap_or(Provider::connect(server.bound_address.to_string()).await?);
 
     let script = build_script(script_binary, tx_params);
 
@@ -63,20 +47,49 @@ fn build_script(script_binary: Vec<u8>, tx_params: TxParameters) -> Script {
 #[cfg(test)]
 mod tests {
     use crate::script::run_compiled_script;
+    use crate::{
+        setup_single_asset_coins, setup_test_provider, DEFAULT_COIN_AMOUNT, DEFAULT_NUM_COINS,
+    };
+    use fuels_core::constants::BASE_ASSET_ID;
     use fuels_core::parameters::TxParameters;
+    use fuels_signers::WalletUnlocked;
     use fuels_types::errors::Error;
 
     #[tokio::test]
     async fn test_run_compiled_script() -> Result<(), Error> {
         // ANCHOR: run_compiled_script
         let path_to_bin = "../fuels/tests/test_projects/logging/out/debug/logging.bin";
-        let return_val = run_compiled_script(path_to_bin, TxParameters::default()).await?;
+        let return_val = run_compiled_script(path_to_bin, TxParameters::default(), None).await?;
 
         let correct_hex =
             hex::decode("ef86afa9696cf0dc6385e2c407a6e159a1103cefb7e2ae0636fb33d3cb2a9e4a")?;
 
         assert_eq!(correct_hex, return_val[0].data().unwrap());
         // ANCHOR_END: run_compiled_script
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_run_compiled_script_with_custom_provider() -> Result<(), Error> {
+        let path_to_bin = "../fuels/tests/test_projects/logging/out/debug/logging.bin";
+
+        let wallet = WalletUnlocked::new_random(None);
+
+        let coins = setup_single_asset_coins(
+            wallet.address(),
+            BASE_ASSET_ID,
+            DEFAULT_NUM_COINS,
+            DEFAULT_COIN_AMOUNT,
+        );
+        let (provider, _) = setup_test_provider(coins, vec![], None).await;
+
+        let return_val =
+            run_compiled_script(path_to_bin, TxParameters::default(), Some(provider)).await?;
+
+        let correct_hex =
+            hex::decode("ef86afa9696cf0dc6385e2c407a6e159a1103cefb7e2ae0636fb33d3cb2a9e4a")?;
+
+        assert_eq!(correct_hex, return_val[0].data().unwrap());
         Ok(())
     }
 }
