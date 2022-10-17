a2891bcc62435ce2f653d0d91ca8716466e83d53
iqdecay <victor@berasconsulting.com>
2022-10-17 16:42:55 +0200

    fix: fix harness name

--- packages/fuels/tests/harness.rs
+++ packages/fuels/tests/harness.rs
@@ -5,7 +5,8 @@ mod from_token; // generating instances from decoded tokens
 mod logs; // parsing and decoding contract logs
 mod predicates; // calling predicates and encoding predicate data
 mod providers; // launching and querying providers
-mod scripts; // building and calling scripts
+               // TODO: rename to `scripts`, see issue #640
+mod script; // building and calling scripts
 mod storage; // storage initialization and forwarding
 mod types; // encoding/decoding of native and custom types
 mod wallets; // wallet creation and balance checks
