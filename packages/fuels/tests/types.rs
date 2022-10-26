77b19ce313c07646a7b02c2b9826fdbbe48a1cba
Ahmed Sagdati <37515857+segfault-magnet@users.noreply.github.com>
2022-10-26 23:48:01 +0200

    fix: Support `untyped raw ptr` (#651)
    The new type is ignored, no codegen is done for it since it is an implementation detail not used in the SDK directly.

--- packages/fuels/tests/types.rs
+++ packages/fuels/tests/types.rs
@@ -1271,21 +1271,15 @@ async fn generics_test() -> anyhow::Result<()> {
 
         let arg1 = MegaExample {
             a: ([Bits256([0; 32]), Bits256([0; 32])], "ab".try_into()?),
-            b: (
+            b: vec![(
                 [EnumWGeneric::b(StructWTupleGeneric {
                     a: (w_arr_generic.clone(), w_arr_generic),
                 })],
                 10u32,
-            ),
+            )],
         };
 
-        let result = contract_methods
-            .complex_test(arg1.clone())
-            .call()
-            .await?
-            .value;
-
-        assert_eq!(result, arg1);
+        contract_methods.complex_test(arg1.clone()).call().await?;
     }
 
     Ok(())
