FuelLabs/fuels-rs

commit 77b19ce313c07646a7b02c2b9826fdbbe48a1cba

fix: Support `untyped raw ptr` (#651)
The new type is ignored, no codegen is done for it since it is an implementation detail not used in the SDK directly.

packages/fuels-core/src/code_gen/resolved_type.rs
@@ -75,12 +75,11 @@ pub(crate) fn resolve_type(
             .flatten()
             .map(|array_type| resolve_type(array_type, types))
             .collect::<Result<Vec<_>, _>>()
+            .expect("Failed to resolve types")
     };
 
     let base_type = types.get(&type_application.type_id).unwrap();
 
-    let components = recursively_resolve(&base_type.components)?;
-    let type_arguments = recursively_resolve(&type_application.type_arguments)?;
     let type_field = base_type.type_field.as_str();
 
     [
@@ -94,13 +93,23 @@ pub(crate) fn resolve_type(
         to_struct,
     ]
     .into_iter()
-    .filter_map(|fun| fun(type_field, &components, &type_arguments))
+    .filter_map(|fun| {
+        fun(
+            type_field,
+            move || recursively_resolve(&base_type.components),
+            move || recursively_resolve(&type_application.type_arguments),
+        )
+    })
     .next()
     .ok_or_else(|| Error::InvalidType(format!("Could not resolve {type_field} to any known type")))
 }
 
-fn to_generic(field: &str, _: &[ResolvedType], _: &[ResolvedType]) -> Option<ResolvedType> {
-    let name = extract_generic_name(field)?;
+fn to_generic(
+    type_field: &str,
+    _: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
+) -> Option<ResolvedType> {
+    let name = extract_generic_name(type_field)?;
 
     let type_name = safe_ident(&name).into_token_stream();
     Some(ResolvedType {
@@ -109,13 +118,17 @@ fn to_generic(field: &str, _: &[ResolvedType], _: &[ResolvedType]) -> Option<Res
     })
 }
 
-fn to_array(field: &str, components: &[ResolvedType], _: &[ResolvedType]) -> Option<ResolvedType> {
-    let len = extract_array_len(field)?;
+fn to_array(
+    type_field: &str,
+    components_supplier: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
+) -> Option<ResolvedType> {
+    let len = extract_array_len(type_field)?;
 
-    let type_inside: TokenStream = match components {
+    let type_inside: TokenStream = match components_supplier().as_slice() {
         [single_type] => Ok(single_type.into()),
-        _ => Err(Error::InvalidData(format!(
-            "Array must have only one component! Actual components: {components:?}"
+        other => Err(Error::InvalidData(format!(
+            "Array must have only one component! Actual components: {other:?}"
         ))),
     }
     .unwrap();
@@ -127,11 +140,11 @@ fn to_array(field: &str, components: &[ResolvedType], _: &[ResolvedType]) -> Opt
 }
 
 fn to_sized_ascii_string(
-    field: &str,
-    _: &[ResolvedType],
-    _: &[ResolvedType],
+    type_field: &str,
+    _: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
 ) -> Option<ResolvedType> {
-    let len = extract_str_len(field)?;
+    let len = extract_str_len(type_field)?;
 
     let generic_params = vec![ResolvedType {
         type_name: quote! {#len},
@@ -144,9 +157,13 @@ fn to_sized_ascii_string(
     })
 }
 
-fn to_tuple(field: &str, components: &[ResolvedType], _: &[ResolvedType]) -> Option<ResolvedType> {
-    if has_tuple_format(field) {
-        let inner_types = components.iter().map(TokenStream::from);
+fn to_tuple(
+    type_field: &str,
+    components_supplier: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
+) -> Option<ResolvedType> {
+    if has_tuple_format(type_field) {
+        let inner_types = components_supplier().into_iter().map(TokenStream::from);
 
         // it is important to leave a trailing comma because a tuple with
         // one element is written as (element,) not (element) which is
@@ -162,8 +179,8 @@ fn to_tuple(field: &str, components: &[ResolvedType], _: &[ResolvedType]) -> Opt
 
 fn to_simple_type(
     type_field: &str,
-    _: &[ResolvedType],
-    _: &[ResolvedType],
+    _: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
 ) -> Option<ResolvedType> {
     match type_field {
         "u8" | "u16" | "u32" | "u64" | "bool" | "()" => {
@@ -180,7 +197,11 @@ fn to_simple_type(
     }
 }
 
-fn to_byte(type_field: &str, _: &[ResolvedType], _: &[ResolvedType]) -> Option<ResolvedType> {
+fn to_byte(
+    type_field: &str,
+    _: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
+) -> Option<ResolvedType> {
     if type_field == "byte" {
         let type_name = quote! {Byte};
         Some(ResolvedType {
@@ -191,7 +212,11 @@ fn to_byte(type_field: &str, _: &[ResolvedType], _: &[ResolvedType]) -> Option<R
         None
     }
 }
-fn to_bits256(type_field: &str, _: &[ResolvedType], _: &[ResolvedType]) -> Option<ResolvedType> {
+fn to_bits256(
+    type_field: &str,
+    _: impl Fn() -> Vec<ResolvedType>,
+    _: impl Fn() -> Vec<ResolvedType>,
+) -> Option<ResolvedType> {
     if type_field == "b256" {
         let type_name = quote! {Bits256};
         Some(ResolvedType {
@@ -204,15 +229,15 @@ fn to_bits256(type_field: &str, _: &[ResolvedType], _: &[ResolvedType]) -> Optio
 }
 
 fn to_struct(
-    field_name: &str,
-    _: &[ResolvedType],
-    type_arguments: &[ResolvedType],
+    type_field: &str,
+    _: impl Fn() -> Vec<ResolvedType>,
+    type_arguments_supplier: impl Fn() -> Vec<ResolvedType>,
 ) -> Option<ResolvedType> {
-    custom_type_name(field_name)
+    custom_type_name(type_field)
         .ok()
         .map(|type_name| ident(&type_name))
         .map(|type_name| ResolvedType {
             type_name: type_name.into_token_stream(),
-            generic_params: type_arguments.to_vec(),
+            generic_params: type_arguments_supplier(),
         })
 }

.github/workflows/ci.yml
@@ -18,7 +18,7 @@ env:
   RUSTFLAGS: "-D warnings"
   FUEL_CORE_VERSION: 0.11.2
   RUST_VERSION: 1.64.0
-  FORC_VERSION: 0.26.0
+  FORC_VERSION: 0.28.1
 
 jobs:
   setup-test-projects:

examples/debugging/src/lib.rs
@@ -16,7 +16,7 @@ mod tests {
 
         let selector = resolve_fn_selector(fn_name, &inputs);
 
-        assert_eq!(selector, [0, 0, 0, 0, 93, 206, 142, 135]);
+        assert_eq!(selector, [0, 0, 0, 0, 7, 161, 3, 203]);
         // ANCHOR_END: example_fn_selector
     }
 

packages/fuels-core/src/code_gen/abigen.rs
@@ -220,10 +220,7 @@ impl Abigen {
                 continue;
             }
 
-            // Skip custom type generation if the custom type is a native type.
-            // This means ABI methods receiving or returning a native type
-            // can receive or return that native type directly.
-            if Abigen::is_native_type(&prop.type_field)? {
+            if Abigen::should_skip_codegen(&prop.type_field)? {
                 continue;
             }
 
@@ -236,15 +233,16 @@ impl Abigen {
         Ok(structs)
     }
 
-    // Checks whether the given type field is a native type.
-    // It's expected to come in as `"struct T"` or `"enum T"`.
-    // `T` is a native `high-level language` or Rust type if it matches exactly one of
-    // the reserved strings, such as "Address", "ContractId", "Option" or "Result"
-    pub fn is_native_type(type_field: &str) -> anyhow::Result<bool> {
-        let name = custom_type_name(type_field)?;
+    // Checks whether the given type should not have code generated for it. This
+    // is mainly because the corresponding type in Rust already exists --
+    // e.g. the contract's Vec type is mapped to std::vec::Vec from the Rust
+    // stdlib, ContractId is a custom type implemented by fuels-rs, etc.
+    // Others like 'raw untyped ptr' or 'RawVec' are skipped because they are
+    // implementation details of the contract's Vec type and are not directly
+    // used in the SDK.
+    pub fn should_skip_codegen(type_field: &str) -> anyhow::Result<bool> {
+        let name = custom_type_name(type_field).unwrap_or_else(|_| type_field.to_string());
 
-        // "RawVec" is part of the Vec structure. Not used in the SDK and thus
-        // not generated.
         Ok([
             "ContractId",
             "Address",
@@ -252,6 +250,7 @@ impl Abigen {
             "Identity",
             "Result",
             "Vec",
+            "raw untyped ptr",
             "RawVec",
         ]
         .into_iter()
@@ -265,7 +264,7 @@ impl Abigen {
         let mut seen_enum: Vec<&str> = vec![];
 
         for prop in &self.abi.types {
-            if !prop.is_enum_type() || Abigen::is_native_type(&prop.type_field)? {
+            if !prop.is_enum_type() || Abigen::should_skip_codegen(&prop.type_field)? {
                 continue;
             }
 

packages/fuels-core/src/code_gen/function_selector.rs
@@ -66,7 +66,7 @@ fn resolve_arg(arg: &ParamType) -> String {
         }
         ParamType::Vector(el_type) => {
             let inner = resolve_arg(el_type);
-            format!("s<{inner}>(s<{inner}>(u64,u64),u64)")
+            format!("s<{inner}>(s<{inner}>(rawptr,u64),u64)")
         }
     }
 }
@@ -126,6 +126,15 @@ mod tests {
         let selector = resolve_fn_signature("some_fun", &inputs);
 
         assert_eq!(selector, format!("some_fun(s<u32>(u64,u32))"));
+    }
+
+    #[test]
+    fn handles_vectors() {
+        let inputs = [ParamType::Vector(Box::new(ParamType::U32))];
+
+        let selector = resolve_fn_signature("some_fun", &inputs);
+
+        assert_eq!(selector, "some_fun(s<u32>(s<u32>(rawptr,u64),u64))")
     }
 
     #[test]

packages/fuels-types/src/param_types.rs
@@ -242,7 +242,7 @@ impl TryFrom<&Type> for ParamType {
     type Error = Error;
 
     fn try_from(the_type: &Type) -> Result<Self, Self::Error> {
-        let mut matching_param_types = [
+        let matched_param_type = [
             try_primitive,
             try_array,
             try_str,
@@ -253,16 +253,15 @@ impl TryFrom<&Type> for ParamType {
         ]
         .into_iter()
         .map(|fun| fun(the_type))
-        .flatten_ok()
-        .collect::<Result<Vec<_>, _>>()?;
+        .flat_map(|result| result.ok().flatten())
+        .next();
 
-        if matching_param_types.is_empty() {
-            Err(Error::InvalidType(
-                "Type didn't match against any known ParamTypes".to_string(),
-            ))
-        } else {
-            Ok(matching_param_types.remove(0))
-        }
+        matched_param_type.map(Ok).unwrap_or_else(|| {
+            Err(Error::InvalidType(format!(
+                "Type {} couldn't be converted into a ParamType",
+                the_type.type_field
+            )))
+        })
     }
 }
 
@@ -542,28 +541,34 @@ mod tests {
             },
             TypeDeclaration {
                 type_id: 2,
+                type_field: "raw untyped ptr".to_string(),
+                components: None,
+                type_parameters: None,
+            },
+            TypeDeclaration {
+                type_id: 3,
                 type_field: "struct RawVec".to_string(),
                 components: Some(vec![
                     TypeApplication {
                         name: "ptr".to_string(),
-                        type_id: 4,
+                        type_id: 2,
                         type_arguments: None,
                     },
                     TypeApplication {
                         name: "cap".to_string(),
-                        type_id: 4,
+                        type_id: 5,
                         type_arguments: None,
                     },
                 ]),
                 type_parameters: Some(vec![1]),
             },
             TypeDeclaration {
-                type_id: 3,
+                type_id: 4,
                 type_field: "struct Vec".to_string(),
                 components: Some(vec![
                     TypeApplication {
                         name: "buf".to_string(),
-                        type_id: 2,
+                        type_id: 3,
                         type_arguments: Some(vec![TypeApplication {
                             name: "".to_string(),
                             type_id: 1,
@@ -572,20 +577,20 @@ mod tests {
                     },
                     TypeApplication {
                         name: "len".to_string(),
-                        type_id: 4,
+                        type_id: 5,
                         type_arguments: None,
                     },
                 ]),
                 type_parameters: Some(vec![1]),
             },
             TypeDeclaration {
-                type_id: 4,
+                type_id: 5,
                 type_field: "u64".to_string(),
                 components: None,
                 type_parameters: None,
             },
             TypeDeclaration {
-                type_id: 5,
+                type_id: 6,
                 type_field: "u8".to_string(),
                 components: None,
                 type_parameters: None,
@@ -594,10 +599,10 @@ mod tests {
 
         let type_application = TypeApplication {
             name: "arg".to_string(),
-            type_id: 3,
+            type_id: 4,
             type_arguments: Some(vec![TypeApplication {
                 name: "".to_string(),
-                type_id: 5,
+                type_id: 6,
                 type_arguments: None,
             }]),
         };
@@ -816,12 +821,12 @@ mod tests {
                 components: Some(vec![
                     TypeApplication {
                         name: "__tuple_element".to_string(),
-                        type_id: 5,
+                        type_id: 4,
                         type_arguments: None,
                     },
                     TypeApplication {
                         name: "__tuple_element".to_string(),
-                        type_id: 13,
+                        type_id: 24,
                         type_arguments: None,
                     },
                 ]),
@@ -833,12 +838,12 @@ mod tests {
                 components: Some(vec![
                     TypeApplication {
                         name: "__tuple_element".to_string(),
-                        type_id: 4,
+                        type_id: 5,
                         type_arguments: None,
                     },
                     TypeApplication {
                         name: "__tuple_element".to_string(),
-                        type_id: 23,
+                        type_id: 13,
                         type_arguments: None,
                     },
                 ]),
@@ -852,13 +857,13 @@ mod tests {
                     type_id: 8,
                     type_arguments: Some(vec![TypeApplication {
                         name: "".to_string(),
-                        type_id: 21,
+                        type_id: 22,
                         type_arguments: Some(vec![TypeApplication {
                             name: "".to_string(),
-                            type_id: 20,
+                            type_id: 21,
                             type_arguments: Some(vec![TypeApplication {
                                 name: "".to_string(),
-                                type_id: 17,
+                                type_id: 18,
                                 type_arguments: Some(vec![TypeApplication {
                                     name: "".to_string(),
                                     type_id: 13,
@@ -902,7 +907,7 @@ mod tests {
                 components: Some(vec![
                     TypeApplication {
                         name: "a".to_string(),
-                        type_id: 24,
+                        type_id: 25,
                         type_arguments: None,
                     },
                     TypeApplication {
@@ -951,64 +956,70 @@ mod tests {
             },
             TypeDeclaration {
                 type_id: 15,
-                type_field: "str[2]".to_string(),
+                type_field: "raw untyped ptr".to_string(),
                 components: None,
                 type_parameters: None,
             },
             TypeDeclaration {
                 type_id: 16,
+                type_field: "str[2]".to_string(),
+                components: None,
+                type_parameters: None,
+            },
+            TypeDeclaration {
+                type_id: 17,
                 type_field: "struct MegaExample".to_string(),
                 components: Some(vec![
                     TypeApplication {
                         name: "a".to_string(),
-                        type_id: 2,
+                        type_id: 3,
                         type_arguments: None,
                     },
                     TypeApplication {
                         name: "b".to_string(),
-                        type_id: 3,
-                        type_arguments: None,
+                        type_id: 23,
+                        type_arguments: Some(vec![TypeApplication {
+                            name: "".to_string(),
+                            type_id: 2,
+                            type_arguments: None,
+                        }]),
                     },
                 ]),
                 type_parameters: Some(vec![13, 14]),
             },
             TypeDeclaration {
-                type_id: 17,
+                type_id: 18,
                 type_field: "struct PassTheGenericOn".to_string(),
                 components: Some(vec![TypeApplication {
                     name: "one".to_string(),
-                    type_id: 22,
+                    type_id: 20,
                     type_arguments: Some(vec![TypeApplication {
                         name: "".to_string(),
-                        type_id: 19,
-                        type_arguments: Some(vec![TypeApplication {
-                            name: "".to_string(),
-                            type_id: 9,
-                            type_arguments: None,
-                        }]),
+                        type_id: 9,
+                        type_arguments: None,
                     }]),
                 }]),
                 type_parameters: Some(vec![9]),
             },
             TypeDeclaration {
-                type_id: 18,
+                type_id: 19,
                 type_field: "struct RawVec".to_string(),
                 components: Some(vec![
                     TypeApplication {
                         name: "ptr".to_string(),
-                        type_id: 24,
+                        type_id: 15,
                         type_arguments: None,
                     },
                     TypeApplication {
                         name: "cap".to_string(),
-                        type_id: 24,
+                        type_id: 25,
                         type_arguments: None,
                     },
                 ]),
                 type_parameters: Some(vec![13]),
             },
             TypeDeclaration {
-                type_id: 19,
+                type_id: 20,
                 type_field: "struct SimpleGeneric".to_string(),
                 components: Some(vec![TypeApplication {
                     name: "single_generic_param".to_string(),
@@ -1018,7 +1029,7 @@ mod tests {
                 type_parameters: Some(vec![13]),
             },
             TypeDeclaration {
-                type_id: 20,
+                type_id: 21,
                 type_field: "struct StructWArrayGeneric".to_string(),
                 components: Some(vec![TypeApplication {
                     name: "a".to_string(),
@@ -1028,7 +1039,7 @@ mod tests {
                 type_parameters: Some(vec![10]),
             },
             TypeDeclaration {
-                type_id: 21,
+                type_id: 22,
                 type_field: "struct StructWTupleGeneric".to_string(),
                 components: Some(vec![TypeApplication {
                     name: "a".to_string(),
@@ -1038,12 +1049,12 @@ mod tests {
                 type_parameters: Some(vec![11]),
             },
             TypeDeclaration {
-                type_id: 22,
+                type_id: 23,
                 type_field: "struct Vec".to_string(),
                 components: Some(vec![
                     TypeApplication {
                         name: "buf".to_string(),
-                        type_id: 18,
+                        type_id: 19,
                         type_arguments: Some(vec![TypeApplication {
                             name: "".to_string(),
                             type_id: 13,
@@ -1052,33 +1063,38 @@ mod tests {
                     },
                     TypeApplication {
                         name: "len".to_string(),
-                        type_id: 24,
+                        type_id: 25,
                         type_arguments: None,
                     },
                 ]),
                 type_parameters: Some(vec![13]),
             },
             TypeDeclaration {
-                type_id: 23,
+                type_id: 24,
                 type_field: "u32".to_string(),
                 components: None,
                 type_parameters: None,
             },
             TypeDeclaration {
-                type_id: 24,
+                type_id: 25,
                 type_field: "u64".to_string(),
                 components: None,
                 type_parameters: None,
             },
         ];
 
+        let type_lookup = declarations
+            .into_iter()
+            .map(|decl| (decl.type_id, decl))
+            .collect::<HashMap<_, _>>();
+
         let type_application = TypeApplication {
             name: "arg1".to_string(),
-            type_id: 16,
+            type_id: 17,
             type_arguments: Some(vec![
                 TypeApplication {
                     name: "".to_string(),
-                    type_id: 15,
+                    type_id: 16,
                     type_arguments: None,
                 },
                 TypeApplication {
@@ -1089,58 +1105,56 @@ mod tests {
             ]),
         };
 
-        let type_lookup = declarations
-            .into_iter()
-            .map(|decl| (decl.type_id, decl))
-            .collect::<HashMap<_, _>>();
-
         // when
         let result = ParamType::try_from_type_application(&type_application, &type_lookup)?;
 
         // then
-        let pass_the_generic_on = ParamType::Struct {
-            fields: vec![ParamType::Vector(Box::new(ParamType::Struct {
-                fields: vec![ParamType::String(2)],
+        let expected_param_type = {
+            let pass_the_generic_on = ParamType::Struct {
+                fields: vec![ParamType::Struct {
+                    fields: vec![ParamType::String(2)],
+                    generics: vec![ParamType::String(2)],
+                }],
                 generics: vec![ParamType::String(2)],
-            }))],
-            generics: vec![ParamType::String(2)],
-        };
-        let struct_w_array_generic = ParamType::Struct {
-            fields: vec![ParamType::Array(Box::new(pass_the_generic_on.clone()), 2)],
-            generics: vec![pass_the_generic_on],
-        };
-        let struct_w_tuple_generic = ParamType::Struct {
-            fields: vec![ParamType::Tuple(vec![
-                struct_w_array_generic.clone(),
-                struct_w_array_generic.clone(),
-            ])],
-            generics: vec![struct_w_array_generic],
+            };
+            let struct_w_array_generic = ParamType::Struct {
+                fields: vec![ParamType::Array(Box::from(pass_the_generic_on.clone()), 2)],
+                generics: vec![pass_the_generic_on],
+            };
+            let struct_w_tuple_generic = ParamType::Struct {
+                fields: vec![ParamType::Tuple(vec![
+                    struct_w_array_generic.clone(),
+                    struct_w_array_generic.clone(),
+                ])],
+                generics: vec![struct_w_array_generic],
+            };
+
+            ParamType::Struct {
+                fields: vec![
+                    ParamType::Tuple(vec![
+                        ParamType::Array(Box::from(ParamType::B256), 2),
+                        ParamType::String(2),
+                    ]),
+                    ParamType::Vector(Box::from(ParamType::Tuple(vec![
+                        ParamType::Array(
+                            Box::from(ParamType::Enum {
+                                variants: EnumVariants::new(vec![
+                                    ParamType::U64,
+                                    struct_w_tuple_generic.clone(),
+                                ])
+                                .unwrap(),
+                                generics: vec![struct_w_tuple_generic],
+                            }),
+                            1,
+                        ),
+                        ParamType::U32,
+                    ]))),
+                ],
+                generics: vec![ParamType::String(2), ParamType::B256],
+            }
         };
 
-        let expected = ParamType::Struct {
-            fields: vec![
-                ParamType::Tuple(vec![
-                    ParamType::Array(Box::new(ParamType::B256), 2),
-                    ParamType::String(2),
-                ]),
-                ParamType::Tuple(vec![
-                    ParamType::Array(
-                        Box::new(ParamType::Enum {
-                            variants: EnumVariants::new(vec![
-                                ParamType::U64,
-                                struct_w_tuple_generic.clone(),
-                            ])?,
-                            generics: vec![struct_w_tuple_generic],
-                        }),
-                        1,
-                    ),
-                    ParamType::U32,
-                ]),
-            ],
-            generics: vec![ParamType::String(2), ParamType::B256],
-        };
-
-        assert_eq!(result, expected);
+        assert_eq!(result, expected_param_type);
 
         Ok(())
     }

packages/fuels/tests/types.rs
@@ -1271,21 +1271,15 @@ async fn generics_test() -> anyhow::Result<()> {
 
         let arg1 = MegaExample {
             a: ([Bits256([0; 32]), Bits256([0; 32])], "ab".try_into()?),
-            b: (
+            b: vec![(
                 [EnumWGeneric::b(StructWTupleGeneric {
                     a: (w_arr_generic.clone(), w_arr_generic),
                 })],
                 10u32,
-            ),
+            )],
         };
 
-        let result = contract_methods
-            .complex_test(arg1.clone())
-            .call()
-            .await?
-            .value;
-
-        assert_eq!(result, arg1);
+        contract_methods.complex_test(arg1.clone()).call().await?;
     }
 
     Ok(())

commit 7a9d4e1741bb1dcc9385e5929c2bd4dc7263160b

chore(resolved_type): update vector tests
Caused by https://github.com/FuelLabs/fuels-rs/pull/651

packages/fuels-core/src/code_gen/resolved_type.rs
@@ -373,28 +373,33 @@ mod tests {
                 },
                 TypeDeclaration {
                     type_id: 2,
+                    type_field: "raw untyped ptr".to_string(),
+                    ..Default::default()
+                },
+                TypeDeclaration {
+                    type_id: 3,
                     type_field: "struct RawVec".to_string(),
                     components: Some(vec![
                         TypeApplication {
                             name: "ptr".to_string(),
-                            type_id: 3,
+                            type_id: 2,
                             ..Default::default()
                         },
                         TypeApplication {
                             name: "cap".to_string(),
-                            type_id: 3,
+                            type_id: 4,
                             ..Default::default()
                         },
                     ]),
                     type_parameters: Some(vec![1]),
                 },
                 TypeDeclaration {
-                    type_id: 3,
+                    type_id: 4,
                     type_field: "u64".to_string(),
                     ..Default::default()
                 },
                 TypeDeclaration {
-                    type_id: 4,
+                    type_id: 5,
                     type_field: "u8".to_string(),
                     ..Default::default()
                 },