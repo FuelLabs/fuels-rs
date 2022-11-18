30571264e7f1a54f01435d497a22082e3a5bf9aa
hal3e <hal3e.git@gmail.com>
2022-11-18 16:45:13 +0100

    add unit tests

--- packages/fuels-core/src/lib.rs
+++ packages/fuels-core/src/lib.rs
@@ -236,7 +236,7 @@ fn paramtype_decode_log(param_type: &ParamType, token: &Token) -> Result<String,
             let elements = zip(types, tokens)
                 .map(|(ptype, token)| paramtype_decode_log(ptype, token))
                 .collect::<Result<Vec<_>, _>>()?
-                .join(",");
+                .join(", ");
 
             format!("({elements})")
         }
@@ -247,7 +247,12 @@ fn paramtype_decode_log(param_type: &ParamType, token: &Token) -> Result<String,
 
 #[cfg(test)]
 mod tests {
-    use crate::try_from_bytes;
+    use super::DecodeLog;
+    use crate::{
+        try_from_bytes,
+        types::{Bits256, EvmAddress, SizedAsciiString},
+        Parameterize,
+    };
     use fuel_types::{Address, AssetId, ContractId};
     use fuels_types::{constants::WORD_SIZE, errors::Error};
 
@@ -291,6 +296,90 @@ mod tests {
             try_from_bytes::<AssetId>(&bytes)?,
             AssetId::new(bytes.as_slice().try_into()?)
         );
+        Ok(())
+    }
+
+    #[test]
+    fn test_param_type_decode_log() -> anyhow::Result<()> {
+        {
+            assert_eq!(
+                format!("{:?}", true),
+                bool::param_type().decode_log(&[0, 0, 0, 0, 0, 0, 0, 1])?
+            );
+
+            assert_eq!(
+                format!("{:?}", 128u8),
+                u8::param_type().decode_log(&[0, 0, 0, 0, 0, 0, 0, 128])?
+            );
+
+            assert_eq!(
+                format!("{:?}", 256u16),
+                u16::param_type().decode_log(&[0, 0, 0, 0, 0, 0, 1, 0])?
+            );
+
+            assert_eq!(
+                format!("{:?}", 512u32),
+                u32::param_type().decode_log(&[0, 0, 0, 0, 0, 0, 2, 0])?
+            );
+
+            assert_eq!(
+                format!("{:?}", 1024u64),
+                u64::param_type().decode_log(&[0, 0, 0, 0, 0, 0, 4, 0])?
+            );
+        }
+        {
+            assert_eq!(
+                format!("{:?}", (1, 2)),
+                <(u8, u8)>::param_type()
+                    .decode_log(&[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2])?
+            );
+
+            assert_eq!(
+                format!("{:?}", [3, 4]),
+                <[u64; 2]>::param_type()
+                    .decode_log(&[0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 4])?
+            );
+
+            assert_eq!(
+                format!("{:?}", SizedAsciiString::<4>::new("Fuel".to_string())?),
+                SizedAsciiString::<4>::param_type().decode_log(&[70, 117, 101, 108, 0, 0, 0, 0])?
+            );
+        }
+        {
+            assert_eq!(
+                format!("{:?}", Some(42)),
+                <Option<u64>>::param_type()
+                    .decode_log(&[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 42])?
+            );
+
+            assert_eq!(
+                format!("{:?}", Err::<u64, u64>(42u64)),
+                <Result<u64, u64>>::param_type()
+                    .decode_log(&[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 42])?
+            );
+
+            let bits256 = Bits256([
+                239, 134, 175, 169, 105, 108, 240, 220, 99, 133, 226, 196, 7, 166, 225, 89, 161,
+                16, 60, 239, 183, 226, 174, 6, 54, 251, 51, 211, 203, 42, 158, 74,
+            ]);
+
+            assert_eq!(
+                format!("{:?}", bits256),
+                Bits256::param_type().decode_log(&[
+                    239, 134, 175, 169, 105, 108, 240, 220, 99, 133, 226, 196, 7, 166, 225, 89,
+                    161, 16, 60, 239, 183, 226, 174, 6, 54, 251, 51, 211, 203, 42, 158, 74
+                ])?
+            );
+
+            assert_eq!(
+                format!("{:?}", EvmAddress::from(bits256)),
+                EvmAddress::param_type().decode_log(&[
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 166, 225, 89, 161, 16, 60, 239, 183,
+                    226, 174, 6, 54, 251, 51, 211, 203, 42, 158, 74
+                ])?
+            );
+        }
+
         Ok(())
     }
 }
