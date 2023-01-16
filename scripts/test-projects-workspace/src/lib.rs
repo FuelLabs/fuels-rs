db28ebaf1cbac2733d9bd55aee5e09db200a74e2
Emir Salkic <emirsalkicart@gmail.com>
2023-01-16 14:57:29 +0100

    add fmt function

--- scripts/test-projects-workspace/src/lib.rs
+++ scripts/test-projects-workspace/src/lib.rs
@@ -1,10 +1,8 @@
 extern crate core;
 
-// use forc_pkg::manifest::ManifestFile;
-// use glob::glob;
-// use std::path::PathBuf;
-// use walkdir::WalkDir;
 use forc_pkg::{BuildOpts, Built, PkgOpts};
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 pub fn build(path: Option<String>) -> anyhow::Result<Built> {
     let pkg_opts = forc_pkg::PkgOpts {
@@ -20,28 +18,25 @@ pub fn build(path: Option<String>) -> anyhow::Result<Built> {
     forc_pkg::build_with_options(build_opts)
 }
 
-// pub async fn fmt(path: Option<String>) -> Result<(), anyhow::Error> {
-//
-//     let this_dir = if let Some(ref path) = path {
-//         PathBuf::from(path)
-//     } else {
-//         std::env::current_dir()?
-//     };
-//
-//     let manifest_file = ManifestFile::from_dir(&this_dir)?.member_manifests()?;
-//     let manifest_names = manifest_file.iter().map(|(a, _)| a).collect::<Vec<_>>();
-//
-//     for e in WalkDir::new(path.unwrap()).into_iter().filter_map(|e| e.ok()) {
-//         if e.metadata().unwrap().is_dir() {
-//             println!("{}", e.path().display());
-//         }
-//     }
-//
-//
-// for entry in glob(together.as_str())? {
-//     println!("{}", entry?.display());
-// }
-//
-// Ok(())
-//
-// }
+pub async fn fmt(path: Option<String>) -> Result<(), anyhow::Error> {
+    let this_dir = if let Some(ref path) = path {
+        PathBuf::from(path)
+    } else {
+        std::env::current_dir()?
+    };
+
+    for e in WalkDir::new(this_dir.as_os_str())
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        if e.metadata().unwrap().is_file() && e.file_name() == "Forc.toml" {
+            let parent_dir = e.path().parent().unwrap().as_os_str().to_str().unwrap();
+            tokio::process::Command::new("forc-fmt")
+                .args(["--path", parent_dir, "--check"])
+                .spawn()
+                .expect("error: Couldn't read forc-fmt: No such file or directory. Please check if forc-fmt library is installed.");
+        }
+    }
+
+    Ok(())
+}
--- scripts/test-projects-workspace/src/main.rs
+++ scripts/test-projects-workspace/src/main.rs
@@ -5,5 +5,4 @@ use test_projects_workspace::build;
 #[tokio::main]
 async fn main() {
     build(Some(String::from("packages/fuels/tests"))).expect("failed to build workspace tests");
-    // fmt(Some(String::from("packages/fuels/tests"))).await.expect("failed to build workspace tests"); todo do we need this
 }
