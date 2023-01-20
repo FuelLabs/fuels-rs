cde2ef41e9d6e7b4dc5aab6eac26ae71e4af0838
Emir <emirsalkicart@gmail.com>
2023-01-20 23:55:39 +0100

    feat: improve contract deployment error messages (#786)

--- packages/fuels/tests/contracts.rs
+++ packages/fuels/tests/contracts.rs
@@ -944,3 +944,32 @@ async fn test_contract_call_with_non_default_max_input() -> Result<(), Error> {
 
     Ok(())
 }
+#[tokio::test]
+async fn test_deploy_error_messages() -> Result<(), Error> {
+    let wallet = launch_provider_and_get_wallet().await;
+    let mut response = Contract::deploy(
+        "../../packages/fuels/tests/contracts/contract_test/out/debug/no_file_on_path.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::default(),
+    )
+    .await
+    .expect_err("Should have failed");
+
+    let expected = "Invalid data: Failed to read binary file with path";
+    assert!(response.to_string().starts_with(expected));
+
+    response = Contract::deploy(
+        "../../packages/fuels/tests/contracts/contract_test/out/debug/contract_test.biz",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::default(),
+    )
+    .await
+    .expect_err("Should have failed");
+
+    let expected = "Invalid data: The file extension 'biz' is not recognized. Did you mean '.bin'?";
+    assert!(response.to_string().starts_with(expected));
+
+    Ok(())
+}
