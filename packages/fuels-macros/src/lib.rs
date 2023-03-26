6ecc006ca65ce68dc5a22399b2753e37b5747ed4
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2023-03-26 01:48:43 +0100

    reformat

--- packages/fuels-macros/src/lib.rs
+++ packages/fuels-macros/src/lib.rs
@@ -1,8 +1,6 @@
-use proc_macro::TokenStream;
-
-use syn::{parse_macro_input, DeriveInput};
-
 use fuels_code_gen::Abigen;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 use crate::{
     abigen::MacroAbigenTargets,
