79fd0647dff64584047251a8c9acbcc14c7f3598
Green Baneling <XgreenX9999@gmail.com>
2023-05-05 10:50:18 +0100

    Update ci_checks.sh
    Co-authored-by: Brandon Kite <brandonkite92@gmail.com>

--- ci_checks.sh
+++ ci_checks.sh
@@ -2,7 +2,7 @@
 
 # Requires installed:
 # The latest version of the `forc`,`forc-fmt` and `fuel-core`.
-# `cargo install fuel-core-bin --git https://github.com/FuelLabs/fuel-core --tag v0.18.0 --locked`
+# `cargo install fuel-core-bin --git https://github.com/FuelLabs/fuel-core --tag v0.18.1 --locked`
 # `cargo install forc --git https://github.com/FuelLabs/sway --tag v0.38.0 --locked`
 # `cargo install forc-fmt --git https://github.com/FuelLabs/sway --tag v0.38.0 --locked`
 # Note, if you need a custom branch, you can replace `--tag {RELEASE}` with the `--branch {BRANCH_NAME}`.
