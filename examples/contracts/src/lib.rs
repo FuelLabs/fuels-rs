78135bd8f7c2b54f28c8c9076833caed88951f06
Emir Salkic <emirsalkicart@gmail.com>
2023-05-11 10:26:31 +0200

    fix tests

--- examples/contracts/src/lib.rs
+++ examples/contracts/src/lib.rs
@@ -107,7 +107,7 @@ mod tests {
             .await?;
         // ANCHOR_END: contract_call_cost_estimation
 
-        assert_eq!(transaction_cost.gas_used, 505);
+        assert_eq!(transaction_cost.gas_used, 595);
 
         Ok(())
     }
@@ -676,7 +676,7 @@ mod tests {
             .await?;
         // ANCHOR_END: multi_call_cost_estimation
 
-        assert_eq!(transaction_cost.gas_used, 790);
+        assert_eq!(transaction_cost.gas_used, 964);
 
         Ok(())
     }
