8aa1918370b9f68190da16e604478a1a0e13c24a
hal3e <hal3e.git@gmail.com>
2023-05-21 11:31:30 +0200

    remove dbg!

--- packages/fuels-code-gen/src/program_bindings/utils.rs
+++ packages/fuels-code-gen/src/program_bindings/utils.rs
@@ -157,7 +157,6 @@ pub(crate) fn sdk_provided_custom_types_lookup() -> HashMap<TypePath, TypePath> 
 }
 
 pub(crate) fn find_into_impl_type(ttype: &str) -> Option<TokenStream> {
-    dbg!(&ttype);
     match ttype {
         ":: fuels :: types :: Address" => Some(quote! {::fuels::types::bech32::Bech32Address}),
         ":: fuels :: types :: ContractId" => {
