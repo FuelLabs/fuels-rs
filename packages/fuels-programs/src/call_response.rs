37997ebe541bf9cb8935492c9bb3c5e7969343e0
Oleksii Filonenko <brightone@protonmail.com>
2023-05-22 18:10:46 +0300

    feat(contracts): add tx_id to FuelCallResponse (#734)
    Co-authored-by: iqdecay <victor@berasconsulting.com>

--- packages/fuels-programs/src/call_response.rs
+++ packages/fuels-programs/src/call_response.rs
@@ -1,4 +1,4 @@
-use fuel_tx::Receipt;
+use fuel_tx::{Bytes32, Receipt};
 use fuels_types::{
     errors::Result,
     traits::{Parameterize, Tokenizable},
@@ -18,6 +18,7 @@ pub struct FuelCallResponse<D> {
     pub receipts: Vec<Receipt>,
     pub gas_used: u64,
     pub log_decoder: LogDecoder,
+    pub tx_id: Option<Bytes32>,
 }
 // ANCHOR_END: fuel_call_response
 
@@ -32,12 +33,18 @@ impl<D> FuelCallResponse<D> {
             .expect("could not retrieve gas used from ScriptResult")
     }
 
-    pub fn new(value: D, receipts: Vec<Receipt>, log_decoder: LogDecoder) -> Self {
+    pub fn new(
+        value: D,
+        receipts: Vec<Receipt>,
+        log_decoder: LogDecoder,
+        tx_id: Option<Bytes32>,
+    ) -> Self {
         Self {
             value,
             gas_used: Self::get_gas_used(&receipts),
             receipts,
             log_decoder,
+            tx_id,
         }
     }
 
