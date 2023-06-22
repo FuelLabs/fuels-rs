53cb877c33a94a1778b7fe1ff8128e223a173d9a
Emir Salkic <emirsalkicart@gmail.com>
2023-06-22 15:19:52 +0200

    refactor

--- packages/fuels-test-helpers/src/utils.rs
+++ packages/fuels-test-helpers/src/utils.rs
@@ -1,11 +1,9 @@
-use cargo_metadata::Metadata;
 use fuel_core_chain_config::{CoinConfig, MessageConfig};
 use fuels_core::types::{coin::Coin, message::Message};
 use isahc::ReadResponseExt;
 use serde_json::Value;
 use std::env;
 use tokio::process::Command;
-// use toml::Value as TomlValue;
 
 pub fn into_coin_configs(coins: Vec<Coin>) -> Vec<CoinConfig> {
     coins
@@ -30,38 +28,69 @@ pub async fn check_fuel_core_dependency_version() {
     let binary_name = "fuel-core";
 
     let used_fuel_version = if cfg!(not(feature = "fuel-core-lib")) {
-        let path = which::which(binary_name)
-            .unwrap_or_else(|_| panic!("failed to list '{binary_name}' binaries"));
+        let path = match which::which(binary_name) {
+            Ok(path) => path,
+            Err(err) => {
+                eprintln!("Failed to fetch dependency information: {}", err);
+                return;
+            }
+        };
 
-        let output = Command::new(path)
-            .arg("--version")
-            .output()
-            .await
-            .expect("Failed to execute command");
+        let output = match Command::new(path).arg("--version").output().await {
+            Ok(output) => output,
+            Err(err) => {
+                eprintln!("Failed to fetch dependency information: {}", err);
+                return;
+            }
+        };
 
-        let version = String::from_utf8_lossy(&output.stdout)
+        let version = match String::from_utf8_lossy(&output.stdout)
             .split_whitespace()
             .nth(1)
-            .expect("Failed to split string")
-            .trim_end()
-            .to_string();
+        {
+            Some(version) => version.trim_end().to_string(),
+            None => {
+                eprintln!("Failed to fetch dependency information");
+                return;
+            }
+        };
 
         version.split('.').take(2).collect::<Vec<&str>>().join(".")
     } else {
-        let mut current_dir = env::current_dir().expect("Failed to get current directory");
-        current_dir.pop();
-        current_dir.pop();
+        let current_dir = match env::current_dir() {
+            Ok(mut current_dir) => {
+                current_dir.pop();
+                current_dir.pop();
+                current_dir
+            }
+            Err(err) => {
+                eprintln!("Failed to fetch dependency information: {}", err);
+                return;
+            }
+        };
 
-        let metadata: Metadata = cargo_metadata::MetadataCommand::new()
-            .current_dir(current_dir)
+        let metadata = match cargo_metadata::MetadataCommand::new()
+            .current_dir(&current_dir)
             .exec()
-            .expect("Failed to obtain cargo metadata");
+        {
+            Ok(metadata) => metadata,
+            Err(err) => {
+                eprintln!("Failed to fetch dependency information: {}", err);
+                return;
+            }
+        };
 
-        let current_package = metadata
+        let current_package = match metadata
             .packages
             .iter()
-            .find(|package| package.name == "fuel-core")
-            .expect("Failed to find current package");
+            .find(|package| package.name == binary_name)
+        {
+            Some(package) => package,
+            None => {
+                eprintln!("Failed to fetch dependency information");
+                return;
+            }
+        };
 
         current_package
             .version
@@ -75,51 +104,96 @@ pub async fn check_fuel_core_dependency_version() {
     let crate_name = "fuels";
 
     let crate_info_url = format!("https://crates.io/api/v1/crates/{}", crate_name);
-    let mut crate_info_response =
-        isahc::get(crate_info_url).expect("Failed to fetch crate information");
-    let crate_info_body = crate_info_response
-        .text()
-        .expect("Failed to decode response body");
-    let crate_info_json: Value =
-        serde_json::from_str(&crate_info_body).expect("Failed to parse crate information");
+    let mut crate_info_response = match isahc::get(crate_info_url) {
+        Ok(response) => response,
+        Err(err) => {
+            eprintln!("Failed to fetch dependency information: {}", err);
+            return;
+        }
+    };
 
-    let latest_version = crate_info_json["versions"][0]["num"]
-        .as_str()
-        .expect("Failed to retrieve latest version");
+    let crate_info_body = match crate_info_response.text() {
+        Ok(body) => body,
+        Err(err) => {
+            eprintln!("Failed to fetch dependency information: {}", err);
+            return;
+        }
+    };
+
+    let crate_info_json: Result<Value, serde_json::error::Error> =
+        serde_json::from_str(&crate_info_body);
+
+    let latest_version = match crate_info_json {
+        Ok(ref json) => match json["versions"][0]["num"].as_str() {
+            Some(version) => version,
+            None => {
+                eprintln!("Failed to fetch dependency information");
+                return;
+            }
+        },
+        Err(err) => {
+            eprintln!("Failed to fetch dependency information: {}", err);
+            return;
+        }
+    };
 
     let dependency_info_url = format!(
         "https://crates.io/api/v1/crates/{}/{}/dependencies",
         crate_name, latest_version
     );
-    let mut dependency_info_response =
-        isahc::get(dependency_info_url).expect("Failed to fetch dependency information");
-    let dependency_info_body = dependency_info_response
-        .text()
-        .expect("Failed to decode response body");
-    let dependency_info_json: Value = serde_json::from_str(&dependency_info_body)
-        .expect("Failed to parse dependency information");
 
-    if let Some(dependencies) = dependency_info_json["dependencies"].as_array() {
-        if let Some(fuel_core_dependency) = dependencies
-            .iter()
-            .find(|dependency| dependency["crate_id"].as_str() == Some("fuel-core"))
-        {
-            let fuel_core_req_vec = fuel_core_dependency["req"]
-                .as_str()
-                .expect("Failed to retrieve fuel-core requirement")
-                .trim_start_matches('^')
-                .to_string();
+    let mut dependency_info_response = match isahc::get(dependency_info_url) {
+        Ok(response) => response,
+        Err(err) => {
+            eprintln!("Failed to fetch dependency information: {}", err);
+            return;
+        }
+    };
 
-            let fuel_core_req = fuel_core_req_vec
-                .split('.')
-                .take(2)
-                .collect::<Vec<&str>>()
-                .join(".");
+    let dependency_info_body = match dependency_info_response.text() {
+        Ok(body) => body,
+        Err(err) => {
+            eprintln!("Failed to fetch dependency information: {}", err);
+            return;
+        }
+    };
 
-            if fuel_core_req != used_fuel_version {
-                eprintln!("Your fuel-core version {} is not equal to the version {} used by fuels crate. This could potentially lead to errors.", used_fuel_version, fuel_core_req);
-                eprintln!("Consider updating your fuel-core version to match the version used by fuels.\n");
+    let dependency_info_json: Result<Value, serde_json::error::Error> =
+        serde_json::from_str(&dependency_info_body);
+
+    if let Ok(json) = dependency_info_json {
+        if let Some(dependencies) = json["dependencies"].as_array() {
+            if let Some(fuel_core_dependency) = dependencies
+                .iter()
+                .find(|dependency| dependency["crate_id"].as_str() == Some("fuel-core"))
+            {
+                let fuel_core_req_vec = match fuel_core_dependency["req"].as_str() {
+                    Some(req) => req.trim_start_matches('^').to_string(),
+                    None => {
+                        eprintln!("Failed to retrieve fuel-core requirement");
+                        return;
+                    }
+                };
+
+                let fuel_core_req = fuel_core_req_vec
+                    .split('.')
+                    .take(2)
+                    .collect::<Vec<&str>>()
+                    .join(".-");
+
+                if fuel_core_req != used_fuel_version {
+                    eprintln!(
+                        "Your fuel-core version {} is not equal to the version {} used by fuels crate. This could potentially lead to errors.",
+                        used_fuel_version, fuel_core_req
+                    );
+                    eprintln!("Consider updating your fuel-core version to match the version used by fuels.\n");
+                }
             }
         }
+    } else {
+        eprintln!(
+            "Failed to parse dependency information: {:?}",
+            dependency_info_json.err()
+        );
     }
 }
