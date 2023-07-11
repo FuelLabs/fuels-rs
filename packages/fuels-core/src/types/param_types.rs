1c95cb81d40887e468d4cacf639874e2ac17501e
iqdecay <victor@berasconsulting.com>
2023-07-11 13:30:46 +0200

    test

--- packages/fuels-core/src/types/param_types.rs
+++ packages/fuels-core/src/types/param_types.rs
@@ -1468,6 +1468,7 @@ mod tests {
         assert_eq!(param_type, ParamType::RawSlice);
     }
 
+    #[test]
     fn try_std_string_correctly_resolves_param_type() {
         let the_type = given_type_with_path("std::string::String");
 
