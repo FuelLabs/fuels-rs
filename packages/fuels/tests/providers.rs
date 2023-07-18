cf1e7962d2abdb75ae0a94782b8f142963993b21
Emir Salkic <emirsalkicart@gmail.com>
2023-07-18 11:32:34 +0200

    fix

--- packages/fuels/tests/providers.rs
+++ packages/fuels/tests/providers.rs
@@ -511,7 +511,9 @@ async fn test_gas_errors() -> Result<()> {
         .await
         .expect_err("should error");
 
-    let expected = "Provider error: Response errors; not enough coins to fit the target";
+    let expected =
+        "Provider error: Client request error: Response errors; not enough coins to fit the target";
+
     assert!(response.to_string().starts_with(expected));
     Ok(())
 }
