69c1bfe21f5982bed46ff54ddda5aedac1f4c89c
MujkicA <ahmedmujkic2@gmail.com>
2023-08-08 10:44:37 +0200

    push

--- packages/fuels/tests/contracts.rs
+++ packages/fuels/tests/contracts.rs
@@ -850,7 +850,7 @@ async fn test_contract_instance_get_balances() -> Result<()> {
     let contract_balances = contract_instance.get_balances().await?;
     assert_eq!(contract_balances.len(), 1);
 
-    let random_asset_balance = contract_balances.get(&random_asset_id.to_string()).unwrap();
+    let random_asset_balance = contract_balances.get(random_asset_id).unwrap();
     assert_eq!(*random_asset_balance, amount);
 
     Ok(())
