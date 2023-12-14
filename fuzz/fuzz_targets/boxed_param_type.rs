52e07a55e13a2f6d5439b1611fd728af9c444a7a
iqdecay <victor@berasconsulting.com>
2023-12-14 19:49:35 +0100

    apply code review suggestions

--- fuzz/fuzz_targets/boxed_param_type.rs
+++ fuzz/fuzz_targets/boxed_param_type.rs
@@ -1,30 +1,13 @@
 #![no_main]
-#[macro_use]
-extern crate libfuzzer_sys;
-extern crate fuels_core;
+mod utils;
+use libfuzzer_sys::fuzz_target;
 
 use fuels_core::{codec::DecoderConfig, types::param_types::ParamType};
 
 fuzz_target!(|param_type: ParamType| {
-    let max_depth = DecoderConfig::default().max_depth;
-    let array = ParamType::Array(Box::new(param_type), 10_000);
+    let array = ParamType::Array(Box::new(param_type.clone()), 10_000);
     let vector = ParamType::Vector(Box::new(param_type));
 
-    let _ = array.get_return_location();
-    let _ = array.compute_encoding_in_bytes();
-    let _ = array.children_need_extra_receipts();
-    let _ = array.validate_is_decodable(max_depth);
-    let _ = array.is_extra_receipt_needed(true);
-    let _ = array.is_extra_receipt_needed(false);
-    let _ = array.heap_inner_element_size(true);
-    let _ = array.heap_inner_element_size(false);
-
-    let _ = vector.get_return_location();
-    let _ = vector.compute_encoding_in_bytes();
-    let _ = vector.children_need_extra_receipts();
-    let _ = vector.validate_is_decodable(max_depth);
-    let _ = vector.is_extra_receipt_needed(true);
-    let _ = vector.is_extra_receipt_needed(false);
-    let _ = vector.heap_inner_element_size(true);
-    let _ = vector.heap_inner_element_size(false);
+    utils::exercise_param_type(array);
+    utils::exercise_param_type(vector);
 });
--- fuzz/fuzz_targets/vector_of_param_type.rs
+++ fuzz/fuzz_targets/vector_of_param_type.rs
@@ -1,13 +1,11 @@
 #![no_main]
-#[macro_use]
-extern crate libfuzzer_sys;
-extern crate fuels_core;
 
+mod utils;
 use fuels_core::{codec::ABIEncoder, types::Token};
 use fuels_test_helpers::{Config, WalletsConfig};
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|param_types: Vec<ParamType>| {
-    let max_depth = DecoderConfig::default().max_depth;
     let struct_of_param_types = ParamType::Struct{
         fields: param_types,
         generics: vec![],
@@ -18,30 +16,7 @@ fuzz_target!(|param_types: Vec<ParamType>| {
         generics: vec![],
     }
 
-    let _ = struct_of_param_types.get_return_location();
-    let _ = struct_of_param_types.compute_encoding_in_bytes();
-    let _ = struct_of_param_types.children_need_extra_receipts();
-    let _ = struct_of_param_types.validate_is_decodable(max_depth);
-    let _ = struct_of_param_types.is_extra_receipt_needed(true);
-    let _ = struct_of_param_types.is_extra_receipt_needed(false);
-    let _ = struct_of_param_types.heap_inner_element_size(true);
-    let _ = struct_of_param_types.heap_inner_element_size(false);
-
-    let _ = tuple.get_return_location();
-    let _ = tuple.compute_encoding_in_bytes();
-    let _ = tuple.children_need_extra_receipts();
-    let _ = tuple.validate_is_decodable(max_depth);
-    let _ = tuple.is_extra_receipt_needed(true);
-    let _ = tuple.is_extra_receipt_needed(false);
-    let _ = tuple.heap_inner_element_size(true);
-    let _ = tuple.heap_inner_element_size(false);
-
-    let _ = enum_of_param_types.get_return_location();
-    let _ = enum_of_param_types.compute_encoding_in_bytes();
-    let _ = enum_of_param_types.children_need_extra_receipts();
-    let _ = enum_of_param_types.validate_is_decodable(max_depth);
-    let _ = enum_of_param_types.is_extra_receipt_needed(true);
-    let _ = enum_of_param_types.is_extra_receipt_needed(false);
-    let _ = enum_of_param_types.heap_inner_element_size(true);
-    let _ = enum_of_param_types.heap_inner_element_size(false);
+    utils::exercise_param_type(struct_of_param_types);
+    utils::exercise_param_type(tuple);
+    utils::exercise_param_type(enum_of_param_types);
 });
