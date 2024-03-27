0bb6ec03f8ee7dc5fb2fdda3181a2a069383d691
hal3e <git@hal3e.io>
2024-03-27 12:11:27 +0100

    fix imports

--- examples/debugging/src/lib.rs
+++ examples/debugging/src/lib.rs
@@ -1,10 +1,13 @@
 #[cfg(test)]
 mod tests {
+    #[cfg(not(feature = "experimental"))]
     use std::collections::HashMap;
 
+    #[cfg(not(feature = "experimental"))]
     use fuel_abi_types::abi::program::ProgramABI;
     #[cfg(not(feature = "experimental"))]
     use fuels::core::codec::{calldata, fn_selector};
+    #[cfg(not(feature = "experimental"))]
     use fuels::{
         //core::codec::ABIDecoder, // TODO: @hal3e temporarily disabled
         //macros::abigen, // TODO: @hal3e temporarily disabled
