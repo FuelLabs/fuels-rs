dc1066d2dba09831bdff974b8572c33f7c553b65
Oleksii Filonenko <12615679+Br1ght0ne@users.noreply.github.com>
2024-04-12 13:01:43 +0300

    off by one omg

--- packages/fuels/tests/providers.rs
+++ packages/fuels/tests/providers.rs
@@ -333,7 +333,7 @@ async fn test_gas_forwarded_defaults_to_tx_limit() -> Result<()> {
     #[cfg(not(feature = "experimental"))]
     let gas_used_by_script = 209;
     #[cfg(feature = "experimental")]
-    let gas_used_by_script = 243;
+    let gas_used_by_script = 242;
     let gas_limit = 225_883;
     let response = contract_instance
         .methods()
