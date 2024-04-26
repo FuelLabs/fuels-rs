80c800daf5d7dae8e5c4f6ee323882dff940e51b
hal3e <git@hal3e.io>
2024-04-26 11:44:40 +0200

    fmt

--- packages/fuels-code-gen/src/utils.rs
+++ packages/fuels-code-gen/src/utils.rs
@@ -3,8 +3,9 @@ pub use source::Source;
 
 mod source;
 
-use crate::{error, error::Result, Abi};
 use fuel_abi_types::abi::full_program::FullProgramABI;
+
+use crate::{error, error::Result, Abi};
 
 pub fn parse_program_abi(abi: &str) -> Result<Abi> {
     let source = Source::parse(abi)?;
