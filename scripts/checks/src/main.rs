88bbec3900f2212300c4875e1dd12dc331f68d9b
Ahmed Sagdati <ahmed.sagdati@fuel.sh>
2024-05-12 09:18:19 +0200

    checks tool wip

---
+++ scripts/checks/src/main.rs
@@ -0,0 +1,20 @@
+use std::io::IsTerminal;
+
+use clap::Parser;
+mod cli;
+mod config;
+mod task;
+mod util;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = cli::Cli::parse();
+    let verbose = cli.verbose;
+    let tasks = util::read_tasks_from_config(cli)?;
+
+    let is_tty = std::io::stderr().is_terminal();
+
+    tasks.run(is_tty, verbose).await?;
+
+    Ok(())
+}
