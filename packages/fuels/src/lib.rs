17d267159ae76d1d8f21a9aa079d2d608df4db31
Oleksii Filonenko <12615679+Br1ght0ne@users.noreply.github.com>
2024-05-28 16:43:04 +0100

    feat: make `setup_program_test!` macro hygienic (#1389)
    Close #1376.
    
    - I opted for the "hide dependency behind function" approach
    
    ### Checklist
    - [x] I have linked to any relevant issues.
    - [ ] I have updated the documentation.
    - [x] I have added tests that prove my fix is effective or that my
    feature works.
    - [x] I have added necessary labels.
    - [x] I have done my best to ensure that my PR adheres to [the Fuel Labs
    Code Review
    Standards](https://github.com/FuelLabs/rfcs/blob/master/text/code-standards/external-contributors.md).
    - [x] I have requested a review from the relevant team or maintainers.

--- packages/fuels/src/lib.rs
+++ packages/fuels/src/lib.rs
@@ -72,6 +72,7 @@ pub mod prelude {
             codec::{LogDecoder, LogId, LogResult},
             traits::Signer,
         },
+        macros::setup_program_test,
         programs::{
             call_utils::TxDependencyExtension,
             contract::{
@@ -84,7 +85,7 @@ pub mod prelude {
     };
     pub use super::{
         core::constants::*,
-        macros::{abigen, setup_program_test},
+        macros::abigen,
         tx::Receipt,
         types::{
             bech32::{Bech32Address, Bech32ContractId},
