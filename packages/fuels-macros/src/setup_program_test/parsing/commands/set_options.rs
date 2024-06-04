d8ecdbbef818c56b18393d4362a10b71104032e9
Oleksii Filonenko <12615679+Br1ght0ne@users.noreply.github.com>
2024-06-04 02:56:08 +0100

    deps: remove strum, impl string traits

--- packages/fuels-macros/src/setup_program_test/parsing/commands/set_options.rs
+++ packages/fuels-macros/src/setup_program_test/parsing/commands/set_options.rs
@@ -1,16 +1,39 @@
-use std::convert::TryFrom;
+use std::{convert::TryFrom, fmt, str::FromStr};
 
-use strum::{Display, EnumString};
 use syn::Error;
 
 use crate::parse_utils::{Command, UniqueNameValues};
 
-#[derive(Debug, Clone, Default, PartialEq, Eq, EnumString, Display)]
-#[strum(serialize_all = "lowercase")]
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub enum BuildProfile {
     Debug,
     #[default]
     Release,
+}
+
+impl FromStr for BuildProfile {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "debug" => Ok(Self::Debug),
+            "release" => Ok(Self::Release),
+            _ => Err(r#"invalid build profile option: must be "default" or "release""#),
+        }
+    }
+}
+
+impl fmt::Display for BuildProfile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                BuildProfile::Debug => "debug",
+                BuildProfile::Release => "release",
+            }
+        )
+    }
 }
 
 #[derive(Debug, Clone, Default)]
