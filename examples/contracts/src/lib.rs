a10739afbe9ad9e869b2c873e50adb5a8d23a056
MujkicA <ahmedmujkic2@gmail.com>
2024-06-11 18:13:28 +0200

    fix gas tests

--- examples/contracts/src/lib.rs
+++ examples/contracts/src/lib.rs
@@ -113,7 +113,7 @@ mod tests {
             .await?;
         // ANCHOR_END: contract_call_cost_estimation
 
-        let expected_gas = 2606;
+        let expected_gas = 2613;
 
         assert_eq!(transaction_cost.gas_used, expected_gas);
 
@@ -611,7 +611,7 @@ mod tests {
             .await?;
         // ANCHOR_END: multi_call_cost_estimation
 
-        let expected_gas = 4051;
+        let expected_gas = 4063;
 
         assert_eq!(transaction_cost.gas_used, expected_gas);
 
