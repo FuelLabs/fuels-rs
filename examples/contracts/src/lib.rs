73b907d93dfda87cfd5a2e5da1b2070da4683684
hal3e <git@hal3e.io>
2024-07-04 13:43:33 +0200

    chore: use `forc` `0.61.2` (#1453)

--- examples/contracts/src/lib.rs
+++ examples/contracts/src/lib.rs
@@ -113,7 +113,7 @@ mod tests {
             .await?;
         // ANCHOR_END: contract_call_cost_estimation
 
-        let expected_gas = 2609;
+        let expected_gas = 2617;
 
         assert_eq!(transaction_cost.gas_used, expected_gas);
 
@@ -610,7 +610,7 @@ mod tests {
             .await?;
         // ANCHOR_END: multi_call_cost_estimation
 
-        let expected_gas = 4076;
+        let expected_gas = 4067;
 
         assert_eq!(transaction_cost.gas_used, expected_gas);
 
