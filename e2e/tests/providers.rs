04dbe5f2e9756b3c9ab227dc6876361633ace65e
hal3e <git@hal3e.io>
2024-07-19 10:39:05 +0200

    chore: use `forc` `0.62.0`

--- e2e/tests/providers.rs
+++ e2e/tests/providers.rs
@@ -331,7 +331,7 @@ async fn test_gas_forwarded_defaults_to_tx_limit() -> Result<()> {
     );
 
     // The gas used by the script to call a contract and forward remaining gas limit.
-    let gas_used_by_script = 248;
+    let gas_used_by_script = 249;
     let gas_limit = 225_883;
     let response = contract_instance
         .methods()
