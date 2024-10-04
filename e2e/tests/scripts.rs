55864a76ee70ae6828802e6a98ca4f0ce9e01a2d
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2024-10-04 23:50:49 +0200

    type state pattern for executable

--- e2e/tests/scripts.rs
+++ e2e/tests/scripts.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use fuel_asm::{op, Instruction, RegId};
 use fuels::{
     core::{
@@ -5,6 +7,7 @@ use fuels::{
         Configurables,
     },
     prelude::*,
+    programs::contract::{Executable, Regular},
     types::{Identity, Token},
 };
 
@@ -402,116 +405,6 @@ async fn simulations_can_be_made_without_coins() -> Result<()> {
     Ok(())
 }
 
-fn get_data_offset(binary: &[u8]) -> usize {
-    let data_offset: [u8; 8] = binary[8..16].try_into().unwrap();
-    u64::from_be_bytes(data_offset) as usize
-}
-
-fn new_loader(
-    mut original_binary: Vec<u8>,
-    configurables: impl Into<Configurables>,
-    blob_id: &BlobId,
-) -> Result<Vec<u8>> {
-    // The final code is going to have this structure:
-    // 1. loader instructions
-    // 2. blob id
-    // 3. length_of_data_section
-    // 4. the data_section (updated with configurables as needed)
-
-    let configurables: Configurables = configurables.into();
-    configurables.update_constants_in(&mut original_binary);
-
-    let offset = get_data_offset(&original_binary);
-
-    let data_section = original_binary[offset..].to_vec();
-
-    // update the data_section here as necessary (with configurables)
-
-    let data_section_len = data_section.len();
-
-    const BLOB_ID_SIZE: u16 = 32;
-    const REG_ADDRESS_OF_DATA_AFTER_CODE: u8 = 0x10;
-    const REG_START_OF_LOADED_CODE: u8 = 0x11;
-    const REG_GENERAL_USE: u8 = 0x12;
-    const REG_START_OF_DATA_SECTION: u8 = 0x13;
-    let get_instructions = |num_of_instructions| {
-        // There are 3 main steps:
-        // 1. Load the blob content into memory
-        // 2. Load the data section right after the blob
-        // 3. Jump to the beginning of the memory where the blob was loaded
-        [
-            // 1. Load the blob content into memory
-            // Find the start of the hardcoded blob ID, which is located after the loader code ends.
-            op::move_(REG_ADDRESS_OF_DATA_AFTER_CODE, RegId::PC),
-            // hold the address of the blob ID.
-            op::addi(
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                num_of_instructions * Instruction::SIZE as u16,
-            ),
-            // The code is going to be loaded from the current value of SP onwards, save
-            // the location into REG_START_OF_LOADED_CODE so we can jump into it at the end.
-            op::move_(REG_START_OF_LOADED_CODE, RegId::SP),
-            // REG_GENERAL_USE to hold the size of the blob.
-            op::bsiz(REG_GENERAL_USE, REG_ADDRESS_OF_DATA_AFTER_CODE),
-            // Push the blob contents onto the stack.
-            op::ldc(REG_ADDRESS_OF_DATA_AFTER_CODE, 0, REG_GENERAL_USE, 1),
-            // Move on to the data section length
-            op::addi(
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                BLOB_ID_SIZE,
-            ),
-            // load the size of the data section into REG_GENERAL_USE
-            op::lw(REG_GENERAL_USE, REG_ADDRESS_OF_DATA_AFTER_CODE, 0),
-            // after we have read the length of the data section, we move the pointer to the actual
-            // data by skipping WORD_SIZE B.
-            op::addi(
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                WORD_SIZE as u16,
-            ),
-            // extend the stack
-            op::cfe(REG_GENERAL_USE),
-            // move to the start of the newly allocated stack
-            op::sub(REG_START_OF_DATA_SECTION, RegId::SP, REG_GENERAL_USE),
-            // load the data section onto the stack
-            op::mcp(
-                REG_START_OF_DATA_SECTION,
-                REG_ADDRESS_OF_DATA_AFTER_CODE,
-                REG_GENERAL_USE,
-            ),
-            // Jump into the memory where the contract is loaded.
-            // What follows is called _jmp_mem by the sway compiler.
-            // Subtract the address contained in IS because jmp will add it back.
-            op::sub(
-                REG_START_OF_LOADED_CODE,
-                REG_START_OF_LOADED_CODE,
-                RegId::IS,
-            ),
-            // jmp will multiply by 4, so we need to divide to cancel that out.
-            op::divi(REG_START_OF_LOADED_CODE, REG_START_OF_LOADED_CODE, 4),
-            // Jump to the start of the contract we loaded.
-            op::jmp(REG_START_OF_LOADED_CODE),
-        ]
-    };
-
-    let num_of_instructions = u16::try_from(get_instructions(0).len())
-        .expect("to never have more than u16::MAX instructions");
-
-    let instruction_bytes = get_instructions(num_of_instructions)
-        .into_iter()
-        .flat_map(|instruction| instruction.to_bytes());
-
-    let blob_bytes = blob_id.iter().copied();
-
-    Ok(instruction_bytes
-        .chain(blob_bytes)
-        .chain(data_section_len.to_be_bytes())
-        .chain(data_section)
-        .collect())
-}
-
 #[tokio::test]
 async fn can_be_run_in_blobs() -> Result<()> {
     abigen!(Script(
@@ -519,34 +412,23 @@ async fn can_be_run_in_blobs() -> Result<()> {
         name = "MyScript"
     ));
 
-    let binary = std::fs::read("./sway/scripts/script_blobs/out/release/script_blobs.bin").unwrap();
+    let binary_path = "./sway/scripts/script_blobs/out/release/script_blobs.bin";
+    let binary = std::fs::read(binary_path).unwrap();
     let wallet = launch_provider_and_get_wallet().await.unwrap();
     let provider = wallet.provider().unwrap().clone();
 
-    let data_section_offset = get_data_offset(&binary);
-    let blob = Blob::new(binary[..data_section_offset].to_vec());
+    let regular = Executable::load_from(binary_path);
 
-    let blob_id = blob.id();
-
-    let mut blob_tb = BlobTransactionBuilder::default().with_blob(blob);
-
-    wallet.adjust_for_fee(&mut blob_tb, 0).await.unwrap();
-    blob_tb.add_signer(wallet.clone()).unwrap();
-
-    let tx = blob_tb.build(provider.clone()).await.unwrap();
-    provider
-        .send_transaction_and_await_commit(tx)
-        .await
-        .unwrap()
-        .check(None)
-        .unwrap();
+    let code = regular.code();
 
     let configurables = MyScriptConfigurables::default()
         .with_SECRET_NUMBER(10001)
         .unwrap();
-    let new_binary = new_loader(binary, configurables, &blob_id).unwrap();
+    let loader = regular.to_loader().with_configurables(configurables);
 
-    let mut tb = ScriptTransactionBuilder::default().with_script(new_binary);
+    loader.upload_blob(wallet.clone()).await;
+
+    let mut tb = ScriptTransactionBuilder::default().with_script(loader.code());
 
     wallet.adjust_for_fee(&mut tb, 0).await.unwrap();
 
