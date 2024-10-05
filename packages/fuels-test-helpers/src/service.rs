603c297f12d2b7bff2e72fa41d88c3be99919f3c
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2024-10-05 23:32:42 +0200

    tests passing

--- packages/fuels-test-helpers/src/service.rs
+++ packages/fuels-test-helpers/src/service.rs
@@ -91,7 +91,7 @@ impl FuelService {
             graphql_config: GraphQLConfig {
                 addr: node_config.addr,
                 max_queries_depth: 16,
-                max_queries_complexity: 20000,
+                max_queries_complexity: 80000,
                 max_queries_recursive_depth: 16,
                 max_queries_directives: 10,
                 request_body_bytes_limit: 16 * 1024 * 1024,
