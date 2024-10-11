79a2dc614e951f26b2974b29d67d15b7e00a6b54
MujkicA <ahmedmujkic2@gmail.com>
2024-10-11 18:48:10 +0200

    make build script compile for wasm

--- packages/fuels-accounts/build.rs
+++ packages/fuels-accounts/build.rs
@@ -1,17 +1,15 @@
-use std::{env, fs};
-
 fn main() {
-    //
-    let std = env::var("CARGO_FEATURE_STD").is_ok();
+    #[cfg(not(feature = "std"))]
+    {
+        use std::{env, fs};
 
-    if std {
         fs::create_dir_all("target").expect("Unable to create target directory");
         fs::write(
             "target/fuel-core-client-schema.sdl",
             fuel_core_client::SCHEMA_SDL,
         )
         .expect("Unable to write schema file");
-    }
 
-    println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed=build.rs");
+    }
 }
