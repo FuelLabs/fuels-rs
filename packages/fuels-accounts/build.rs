d08e35745bf818efbe428726a90f03f5359b4aaa
MujkicA <ahmedmujkic2@gmail.com>
2024-10-11 18:17:00 +0200

    std check in build script

--- packages/fuels-accounts/build.rs
+++ packages/fuels-accounts/build.rs
@@ -1,13 +1,17 @@
-#![cfg(feature = "std")]
-use std::fs;
+use std::{env, fs};
 
 fn main() {
-    fs::create_dir_all("target").expect("Unable to create target directory");
-    fs::write(
-        "target/fuel-core-client-schema.sdl",
-        fuel_core_client::SCHEMA_SDL,
-    )
-    .expect("Unable to write schema file");
+    //
+    let std = env::var("CARGO_FEATURE_STD").is_ok();
+
+    if std {
+        fs::create_dir_all("target").expect("Unable to create target directory");
+        fs::write(
+            "target/fuel-core-client-schema.sdl",
+            fuel_core_client::SCHEMA_SDL,
+        )
+        .expect("Unable to write schema file");
+    }
 
     println!("cargo:rerun-if-changed=build.rs");
 }
