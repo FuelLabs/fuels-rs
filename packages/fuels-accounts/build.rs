a5b328b8896882eadb9076847ad6c1bb7fb7f4d1
MujkicA <ahmedmujkic2@gmail.com>
2024-10-11 16:45:50 +0200

    add schema build script

---
+++ packages/fuels-accounts/build.rs
@@ -0,0 +1,13 @@
+#![cfg(feature = "std")]
+use std::fs;
+
+fn main() {
+    fs::create_dir_all("target").expect("Unable to create target directory");
+    fs::write(
+        "target/fuel-core-client-schema.sdl",
+        fuel_core_client::SCHEMA_SDL,
+    )
+    .expect("Unable to write schema file");
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
