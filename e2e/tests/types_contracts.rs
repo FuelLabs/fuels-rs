01723fc6ba91acc51d26e223e48f2288814334e4
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2024-10-23 19:07:04 +0200

    can now debug script data for single and multi contract calls

--- e2e/tests/types_contracts.rs
+++ e2e/tests/types_contracts.rs
@@ -7,7 +7,7 @@ use fuel_asm::{
 };
 use fuel_tx::field::ScriptData;
 use fuels::{
-    core::codec::ABIDecoder,
+    core::{codec::ABIDecoder, traits::Tokenizable},
     prelude::*,
     types::{param_types::ParamType, Bits256, EvmAddress, Identity, SizedAsciiString, B512, U256},
 };
@@ -2031,6 +2031,7 @@ mod logic {
         prelude::Result,
         types::{param_types::ParamType, transaction_builders::BlobId},
     };
+    use itertools::Itertools;
 
     pub struct ContractCallDescription {
         pub amount: u64,
@@ -2047,19 +2048,81 @@ mod logic {
         Unknown,
     }
 
-    struct V1CallInstructions {
+    struct ContractCallInstructions {
         instructions: Vec<Instruction>,
     }
 
-    impl V1CallInstructions {
-        pub fn new(instructions: &[Instruction]) -> Option<Self> {
-            if Self::check_gas_fwd_variant(instructions) || Self::check_normal_variant(instructions)
-            {
-                Some(Self {
-                    instructions: Vec::from(instructions),
-                })
+    impl ContractCallInstructions {
+        pub fn new(instructions: &[Instruction]) -> Option<(Self, usize)> {
+            let gas_fwd = Self::check_gas_fwd_variant(instructions);
+            let normal = Self::check_normal_variant(instructions);
+            if gas_fwd || normal {
+                let num_instructions = if gas_fwd {
+                    Self::GAS_FWD_OPCODES.len()
+                } else {
+                    Self::NO_GAS_FWD_OPCODES.len()
+                };
+
+                let instructions: Vec<_> = instructions
+                    .iter()
+                    .take(num_instructions)
+                    .cloned()
+                    .collect();
+                let num_instructions_taken = instructions.len();
+
+                Some((Self { instructions }, num_instructions_taken))
             } else {
                 None
+            }
+        }
+        fn call_data_offset(&self) -> u32 {
+            let Instruction::MOVI(movi) = self.instructions[0] else {
+                panic!("should have validated the first instruction is a MOVI");
+            };
+
+            movi.imm18().into()
+        }
+
+        fn describe_contract_call(&self, script_data: &[u8]) -> ContractCallDescription {
+            let amount = u64::from_be_bytes(script_data[..8].try_into().unwrap());
+            let data = &script_data[8..];
+
+            let asset_id = AssetId::new(data[..32].try_into().unwrap());
+            let data = &data[32..];
+
+            let contract_id = ContractId::new(data[..32].try_into().unwrap());
+            let data = &data[32..];
+
+            let _fn_selector_offset = &data[..8];
+            let data = &data[8..];
+
+            let _encoded_args_offset = &data[..8];
+            let data = &data[8..];
+
+            let fn_selector_len = u64::from_be_bytes(data[..8].try_into().unwrap()) as usize;
+            let data = &data[8..];
+
+            let fn_selector = String::from_utf8(data[..fn_selector_len].to_vec()).unwrap();
+            let data = &data[fn_selector_len..];
+
+            let encoded_args = if self.has_gas_forwarding_instructions() {
+                data[..data.len() - WORD_SIZE].to_vec()
+            } else {
+                data.to_vec()
+            };
+            let data = &data[encoded_args.len()..];
+
+            let gas_forwarded = self
+                .has_gas_forwarding_instructions()
+                .then(|| u64::from_be_bytes(data[..WORD_SIZE].try_into().unwrap()));
+
+            ContractCallDescription {
+                amount,
+                asset_id,
+                contract_id,
+                fn_selector,
+                encoded_args,
+                gas_forwarded,
             }
         }
 
@@ -2130,83 +2193,52 @@ mod logic {
         }
     }
 
-    trait CallDataOffsetExtract {
-        fn call_data_offset(&self) -> u32;
-        fn describe_contract_call<'a>(
-            &self,
-            script_data: &'a [u8],
-        ) -> (ContractCallDescription, &'a [u8]);
-    }
-
-    impl CallDataOffsetExtract for V1CallInstructions {
-        fn call_data_offset(&self) -> u32 {
-            let Instruction::MOVI(movi) = self.instructions[0] else {
-                panic!("should have validated the first instruction is a MOVI");
-            };
-
-            movi.imm18().into()
-        }
-
-        fn describe_contract_call<'a>(
-            &self,
-            script_data: &'a [u8],
-        ) -> (ContractCallDescription, &'a [u8]) {
-            let amount = u64::from_be_bytes(script_data[..8].try_into().unwrap());
-            let data = &script_data[8..];
-
-            let asset_id = AssetId::new(data[..32].try_into().unwrap());
-            let data = &data[32..];
-
-            let contract_id = ContractId::new(data[..32].try_into().unwrap());
-            let data = &data[32..];
-
-            let _fn_selector_offset = &data[..8];
-            let data = &data[8..];
-
-            let _encoded_args_offset = &data[..8];
-            let data = &data[8..];
-
-            let fn_selector_len = u64::from_be_bytes(data[..8].try_into().unwrap()) as usize;
-            let data = &data[8..];
-
-            let fn_selector = String::from_utf8(data[..fn_selector_len].to_vec()).unwrap();
-            let data = &data[fn_selector_len..];
-
-            let encoded_args = if self.has_gas_forwarding_instructions() {
-                data[..data.len() - WORD_SIZE].to_vec()
-            } else {
-                data.to_vec()
-            };
-            let data = &data[encoded_args.len()..];
-
-            let gas_forwarded = self
-                .has_gas_forwarding_instructions()
-                .then(|| u64::from_be_bytes(data[..WORD_SIZE].try_into().unwrap()));
-
-            (
-                ContractCallDescription {
-                    amount,
-                    asset_id,
-                    contract_id,
-                    fn_selector,
-                    encoded_args,
-                    gas_forwarded,
-                },
-                data,
-            )
-        }
-    }
-
     fn parse_contract_calls(
-        instructions: &[Instruction],
+        mut instructions: &[Instruction],
         script_data: &[u8],
     ) -> Option<Vec<ContractCallDescription>> {
-        let instructions = V1CallInstructions::new(instructions).unwrap();
-        let offset = instructions.call_data_offset();
+        let mut call_instructions = vec![];
 
-        let contract_call_description = instructions.describe_contract_call(&script_data).0;
+        while !instructions.is_empty() {
+            match &instructions {
+                [single_instruction] if single_instruction.opcode() == Opcode::RET => break,
+                _ => {}
+            }
 
-        Some(vec![contract_call_description])
+            let (parsed_instructions, amount_read) =
+                ContractCallInstructions::new(instructions).unwrap();
+            instructions = &instructions[amount_read..];
+            call_instructions.push(parsed_instructions);
+        }
+
+        let minimum_call_offset = call_instructions
+            .iter()
+            .map(|i| i.call_data_offset())
+            .min()?;
+
+        let mut descriptions = vec![];
+        let num_calls = call_instructions.len();
+
+        for (idx, current_call_instructions) in call_instructions.iter().enumerate() {
+            let data_start =
+                (current_call_instructions.call_data_offset() - minimum_call_offset) as usize;
+            eprintln!("the offset is {data_start}");
+
+            let data_end = if idx + 1 < num_calls {
+                (call_instructions[idx + 1].call_data_offset()
+                    - current_call_instructions.call_data_offset()) as usize
+            } else {
+                script_data.len()
+            };
+
+            eprintln!("the end is {data_end}");
+
+            let contract_call_description = current_call_instructions
+                .describe_contract_call(&script_data[data_start..data_end]);
+            descriptions.push(contract_call_description);
+        }
+
+        Some(descriptions)
     }
 
     pub fn parse_script(script: &[u8], data: &[u8]) -> Result<ScriptType> {
@@ -2223,7 +2255,7 @@ mod logic {
 }
 
 #[tokio::test]
-async fn can_debug_single_call() -> Result<()> {
+async fn can_debug_single_call_tx() -> Result<()> {
     setup_program_test!(
         Wallets("wallet"),
         Abigen(Contract(
@@ -2234,8 +2266,7 @@ async fn can_debug_single_call() -> Result<()> {
     let contract_id = Contract::load_from(
         "sway/types/contracts/nested_structs/out/release/nested_structs.bin",
         Default::default(),
-    )
-    .unwrap()
+    )?
     .contract_id();
 
     let call_handler = MyContract::new(contract_id, wallet)
@@ -2326,6 +2357,191 @@ async fn can_debug_single_call() -> Result<()> {
         assert_eq!(
             call_description.decode_fn_args(&abi).unwrap(),
             vec!["AllStruct { some_struct: SomeStruct { field: 2, field_2: true } }"]
+        );
+    }
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn can_debug_multi_call_tx() -> Result<()> {
+    setup_program_test!(
+        Wallets("wallet"),
+        Abigen(Contract(
+            name = "MyContract",
+            project = "e2e/sway/types/contracts/nested_structs"
+        ))
+    );
+    let contract_id = Contract::load_from(
+        "sway/types/contracts/nested_structs/out/release/nested_structs.bin",
+        Default::default(),
+    )?
+    .contract_id();
+
+    let call1 = MyContract::new(contract_id, wallet.clone())
+        .methods()
+        .check_struct_integrity(AllStruct {
+            some_struct: SomeStruct {
+                field: 2,
+                field_2: true,
+            },
+        });
+
+    let call2 = MyContract::new(contract_id, wallet.clone())
+        .methods()
+        .i_am_called_differently(
+            AllStruct {
+                some_struct: SomeStruct {
+                    field: 2,
+                    field_2: true,
+                },
+            },
+            MemoryAddress {
+                contract_id,
+                function_selector: 123,
+                function_data: 456,
+            },
+        );
+
+    // without gas forwarding
+    {
+        let first_call = call1
+            .clone()
+            .call_params(CallParameters::default().with_amount(10))
+            .unwrap();
+
+        let second_call = call2
+            .clone()
+            .call_params(CallParameters::default().with_amount(20))
+            .unwrap();
+
+        let tb = CallHandler::new_multi_call(wallet.clone())
+            .add_call(first_call)
+            .add_call(second_call)
+            .transaction_builder()
+            .await
+            .unwrap();
+
+        let script = tb.script;
+        let script_data = tb.script_data;
+
+        let ScriptType::ContractCall(call_descriptions) =
+            logic::parse_script(&script, &script_data)?
+        else {
+            panic!("expected a contract call")
+        };
+
+        assert_eq!(call_descriptions.len(), 2);
+
+        let call_description = &call_descriptions[0];
+
+        assert_eq!(call_description.contract_id, contract_id);
+        assert_eq!(call_description.amount, 10);
+        assert_eq!(call_description.asset_id, AssetId::default());
+        assert_eq!(call_description.fn_selector, "check_struct_integrity");
+        assert!(call_description.gas_forwarded.is_none());
+
+        let abi = std::fs::read_to_string(
+            "./sway/types/contracts/nested_structs/out/release/nested_structs-abi.json",
+        )
+        .unwrap();
+
+        assert_eq!(
+            call_description.decode_fn_args(&abi).unwrap(),
+            vec!["AllStruct { some_struct: SomeStruct { field: 2, field_2: true } }"]
+        );
+
+        let call_description = &call_descriptions[1];
+
+        assert_eq!(call_description.contract_id, contract_id);
+        assert_eq!(call_description.amount, 20);
+        assert_eq!(call_description.asset_id, AssetId::default());
+        assert_eq!(call_description.fn_selector, "i_am_called_differently");
+        assert!(call_description.gas_forwarded.is_none());
+
+        let abi = std::fs::read_to_string(
+            "./sway/types/contracts/nested_structs/out/release/nested_structs-abi.json",
+        )
+        .unwrap();
+
+        assert_eq!(
+            call_description.decode_fn_args(&abi).unwrap(),
+            vec!["AllStruct { some_struct: SomeStruct { field: 2, field_2: true } }", "MemoryAddress { contract_id: std::contract_id::ContractId { bits: Bits256([30, 98, 236, 170, 92, 50, 241, 229, 25, 84, 244, 97, 73, 213, 229, 66, 71, 43, 219, 164, 88, 56, 25, 148, 6, 70, 74, 244, 106, 177, 71, 237]) }, function_selector: 123, function_data: 456 }"]
+        );
+    }
+
+    // with gas forwarding
+    {
+        let first_call = call1
+            .clone()
+            .call_params(
+                CallParameters::default()
+                    .with_amount(10)
+                    .with_gas_forwarded(15),
+            )
+            .unwrap();
+
+        let second_call = call2
+            .clone()
+            .call_params(
+                CallParameters::default()
+                    .with_amount(20)
+                    .with_gas_forwarded(25),
+            )
+            .unwrap();
+
+        let tb = CallHandler::new_multi_call(wallet.clone())
+            .add_call(first_call)
+            .add_call(second_call)
+            .transaction_builder()
+            .await
+            .unwrap();
+
+        let script = tb.script;
+        let script_data = tb.script_data;
+
+        let ScriptType::ContractCall(call_descriptions) =
+            logic::parse_script(&script, &script_data)?
+        else {
+            panic!("expected a contract call")
+        };
+
+        assert_eq!(call_descriptions.len(), 2);
+
+        let call_description = &call_descriptions[0];
+
+        assert_eq!(call_description.contract_id, contract_id);
+        assert_eq!(call_description.amount, 10);
+        assert_eq!(call_description.asset_id, AssetId::default());
+        assert_eq!(call_description.fn_selector, "check_struct_integrity");
+        assert_eq!(call_description.gas_forwarded, Some(15));
+
+        let abi = std::fs::read_to_string(
+            "./sway/types/contracts/nested_structs/out/release/nested_structs-abi.json",
+        )
+        .unwrap();
+
+        assert_eq!(
+            call_description.decode_fn_args(&abi).unwrap(),
+            vec!["AllStruct { some_struct: SomeStruct { field: 2, field_2: true } }"]
+        );
+
+        let call_description = &call_descriptions[1];
+
+        assert_eq!(call_description.contract_id, contract_id);
+        assert_eq!(call_description.amount, 20);
+        assert_eq!(call_description.asset_id, AssetId::default());
+        assert_eq!(call_description.fn_selector, "i_am_called_differently");
+        assert_eq!(call_description.gas_forwarded, Some(25));
+
+        let abi = std::fs::read_to_string(
+            "./sway/types/contracts/nested_structs/out/release/nested_structs-abi.json",
+        )
+        .unwrap();
+
+        assert_eq!(
+            call_description.decode_fn_args(&abi).unwrap(),
+            vec!["AllStruct { some_struct: SomeStruct { field: 2, field_2: true } }", "MemoryAddress { contract_id: std::contract_id::ContractId { bits: Bits256([30, 98, 236, 170, 92, 50, 241, 229, 25, 84, 244, 97, 73, 213, 229, 66, 71, 43, 219, 164, 88, 56, 25, 148, 6, 70, 74, 244, 106, 177, 71, 237]) }, function_selector: 123, function_data: 456 }"]
         );
     }
 
