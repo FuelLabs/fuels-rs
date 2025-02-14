d25098a1c9b3dd7448657450277f2aaf04c108ea
hal3e <git@hal3e.io>
2025-02-14 19:15:11 +0100

    fix test

--- examples/contracts/src/lib.rs
+++ examples/contracts/src/lib.rs
@@ -118,7 +118,7 @@ mod tests {
             .await?;
         // ANCHOR_END: contract_call_cost_estimation
 
-        let expected_gas = 2597;
+        let expected_gas = 2596;
 
         assert_eq!(transaction_cost.gas_used, expected_gas);
 
@@ -636,7 +636,7 @@ mod tests {
             .await?;
         // ANCHOR_END: multi_call_cost_estimation
 
-        let expected_gas = 4202;
+        let expected_gas = 4200;
 
         assert_eq!(transaction_cost.gas_used, expected_gas);
 
