272be4397ac7a20f04fd603b7097cd1c01e605b7
Salka1988 <emirsalkicart@gmail.com>
2025-02-28 15:17:25 +0100

    add

--- e2e/tests/aws.rs
+++ e2e/tests/aws.rs
@@ -1,12 +1,11 @@
 #[cfg(test)]
 mod tests {
+    use std::any::Any;
     use anyhow::Result;
     use e2e::e2e_helpers::start_aws_kms;
-    use fuels::accounts::kms::AwsWallet;
+    use fuels::accounts::kms::{AwsWallet, CredentialsFile};
     use fuels::accounts::{Account, ViewOnlyAccount};
-    use fuels::prelude::{
-        launch_provider_and_get_wallet, AssetId, Contract, LoadConfiguration, TxPolicies,
-    };
+    use fuels::prelude::{launch_provider_and_get_wallet, AssetId, Contract, Error, LoadConfiguration, TxPolicies};
     use fuels::types::errors::Context;
 
     #[tokio::test(flavor = "multi_thread")]
@@ -90,31 +89,9 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn fund_google_wallet() -> Result<()> {
-        // let wallet = launch_provider_and_get_wallet().await?;
-        //
-        // let amount = 500000000;
-        // // let key = kms.create_key().await?;
-        // let address = key.kms_key.address().clone();
-        //
-        // wallet
-        //     .transfer(&address, amount, AssetId::zeroed(), TxPolicies::default())
-        //     .await
-        //     .context("Failed to transfer funds")?;
-        //
-        // // let your_kms_key_id = key.id;
-        // let provider = wallet.provider().expect("No provider found").clone();
-        //
-        // // ANCHOR: use_kms_wallet
-        // // let wallet = AwsWallet::with_kms_key(your_kms_key_id, kms.client(), Some(provider)).await?;
-        // // ANCHOR_END: use_kms_wallet
-        //
-        // let founded_coins = wallet
-        //     .get_coins(AssetId::zeroed())
-        //     .await?
-        //     .first()
-        //     .expect("No coins found")
-        //     .amount;
-        // assert_eq!(founded_coins, 500000000);
-        Ok(())
+            let credentials_file = CredentialsFile::new().await.map_err(|e| Error::Other(format!("Failed to create credentials file: {}", e)))?;
+
+
+            Ok(())
     }
 }
--- packages/fuels-accounts/src/kms/google/client.rs
+++ packages/fuels-accounts/src/kms/google/client.rs
@@ -1,64 +1,6 @@
-use google_cloud_kms::client::google_cloud_auth::credentials::CredentialsFile;
+pub use google_cloud_kms::client::{google_cloud_auth::credentials::CredentialsFile, ClientConfig};
 use google_cloud_kms::client::Client;
-use google_cloud_kms::client::ClientConfig;
-
 use fuels_core::types::errors::{Error, Result};
-
-#[derive(Clone)]
-pub struct GoogleConfig {
-    project_id: String,
-    location: String,
-    credentials_file: Option<String>,
-}
-
-impl GoogleConfig {
-    pub fn new(project_id: String, location: String, credentials_file: Option<String>) -> Self {
-        Self {
-            project_id,
-            location,
-            credentials_file,
-        }
-    }
-
-    pub fn from_environment() -> Self {
-        let project_id = std::env::var("GOOGLE_CLOUD_PROJECT")
-            .expect("GOOGLE_CLOUD_PROJECT environment variable not set");
-        let location =
-            std::env::var("GOOGLE_CLOUD_LOCATION").unwrap_or_else(|_| "global".to_string());
-        let credentials_file = std::env::var("GOOGLE_APPLICATION_CREDENTIALS").ok();
-
-        Self {
-            project_id,
-            location,
-            credentials_file,
-        }
-    }
-
-    #[cfg(feature = "test-helpers")]
-    pub fn for_testing(
-        project_id: String,
-        location: String,
-        credentials_file: Option<String>,
-    ) -> Self {
-        Self {
-            project_id,
-            location,
-            credentials_file,
-        }
-    }
-
-    pub fn project_id(&self) -> &str {
-        &self.project_id
-    }
-
-    pub fn location(&self) -> &str {
-        &self.location
-    }
-
-    pub fn credentials_file(&self) -> Option<&str> {
-        self.credentials_file.as_deref()
-    }
-}
 
 #[derive(Clone, Debug)]
 pub struct GoogleClient {
@@ -66,29 +8,11 @@ pub struct GoogleClient {
 }
 
 impl GoogleClient {
-    pub async fn new(config: GoogleConfig) -> Result<Self> {
-        let mut client_config = ClientConfig::default();
+    pub async fn new(config: ClientConfig) -> Result<Self> {
 
-        if let Some(creds_file) = &config.credentials_file {
-            let creds = CredentialsFile::new_from_file(creds_file.clone())
-                .await
-                .map_err(|e| {
-                    Error::Other(format!(
-                        "Failed to read credentials file {}: {}",
-                        creds_file, e
-                    ))
-                })?;
+        let config_debug = format!("{:?}", config);
 
-            client_config = client_config.with_credentials(creds).await.map_err(|e| {
-                Error::Other(format!(
-                    "Failed to create Google KMS client with credentials file {}: {}",
-                    creds_file, e
-                ))
-            })?;
-        }
-
-        let config_debug = format!("{:?}", client_config);
-        let client = Client::new(client_config).await.map_err(|e| {
+        let client = Client::new(config).await.map_err(|e| {
             Error::Other(format!(
                 "Failed to create Google KMS client with config {}: {}",
                 config_debug, e
@@ -100,19 +24,5 @@ impl GoogleClient {
 
     pub fn inner(&self) -> &Client {
         &self.client
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_kms() {
-        let config = GoogleConfig::new("fuel-dev-451516".to_string(), "location".to_string(), None);
-
-
-        CredentialsFile::new()
-
     }
 }
