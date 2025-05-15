3422bcc8b408a4e4a80e46830e91e0c6f2be94bb
hal3e <git@hal3e.io>
2025-05-15 23:08:13 +0200

    chore!: remove `CoinStatus` and `block_created` from `Coin` (#1664)
    closing: https://github.com/FuelLabs/fuels-rs/issues/1663
    
    # Breaking Changes
    
    removed `CoinStatus` and `created_at` from `Coin`
    
    ```rust
    //old
    pub struct Coin {
        pub amount: u64,
        pub block_created: u32,
        pub asset_id: AssetId,
        pub utxo_id: UtxoId,
        pub owner: Bech32Address,
        pub status: CoinStatus,
    }
    ```
    ```rust
    //new
    pub struct Coin {
        pub amount: u64,
        pub asset_id: AssetId,
        pub utxo_id: UtxoId,
        pub owner: Bech32Address,
    }
    ```

--- packages/fuels-core/src/types/wrappers/coin.rs
+++ packages/fuels-core/src/types/wrappers/coin.rs
@@ -8,32 +8,21 @@ use fuel_core_client::client::types::{
 
 use crate::types::bech32::Bech32Address;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub enum CoinStatus {
-    #[default]
-    Unspent,
-    Spent,
-}
-
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
 pub struct Coin {
     pub amount: u64,
-    pub block_created: u32,
     pub asset_id: AssetId,
     pub utxo_id: UtxoId,
     pub owner: Bech32Address,
-    pub status: CoinStatus,
 }
 
 impl From<ClientCoin> for Coin {
     fn from(coin: ClientCoin) -> Self {
         Self {
             amount: coin.amount,
-            block_created: coin.block_created,
             asset_id: coin.asset_id,
             utxo_id: coin.utxo_id,
             owner: Bech32Address::from(coin.owner),
-            status: CoinStatus::Unspent,
         }
     }
 }
@@ -43,11 +32,10 @@ impl From<Coin> for CoinConfig {
         Self {
             tx_id: *coin.utxo_id.tx_id(),
             output_index: coin.utxo_id.output_index(),
-            tx_pointer_block_height: coin.block_created.into(),
-            tx_pointer_tx_idx: Default::default(),
             owner: coin.owner.into(),
             amount: coin.amount,
             asset_id: coin.asset_id,
+            ..Default::default()
         }
     }
 }
