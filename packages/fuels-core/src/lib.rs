3eb9458b3d031bae8d3c6ee06dba3af6b9243f6f
Luiz Estácio | stacio.eth <luizstacio@gmail.com>
2025-09-11 13:40:32 -0300

    feat: expose offsets_with_data on configurables (#1685)
    - Expose offsets_with_data on Configurables this allows for better DX
    when interacting with the Standard SRC12 that requires configurable to
    be provided as `Vec<(u64, Vec<u8>)>`.
    
    Note: I have made `offsets_with_data` public and also implement `Into`.
    I'm not sure what is the best solution here, so once reviewed it can be
    decided how to procede.
    
    Ex.: 
    
    /// Current way to implement:
    ```rs
    // For this two info requires manual change or reading the JSON
    const PROXY_ORACLE_ID_OFFSET: u64 = 38488;
    const PROXY_OWNER_OFFSET: u64 = 38440;
    
    pub fn build_trade_account_proxy_configurables(
        oracle_id: ContractId,
        owner: Identity,
    ) -> Vec<(u64, Vec<u8>)> {
        let mut my_configurables: Vec<(u64, Vec<u8>)> = Vec::with_capacity(2);
    
        my_configurables.push((PROXY_ORACLE_ID_OFFSET, oracle_id.to_vec()));
    
        let mut owner_data: Vec<u8> = Vec::new();
        owner_data.extend_from_slice(&[0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 1u8]); // State::Initialized
        match owner {
            Identity::Address(address) => {
                owner_data.extend_from_slice(&[0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8]); // Identity::Address
                owner_data.extend_from_slice(&*address.to_vec());
            }
            Identity::ContractId(contract_id) => {
                owner_data.extend_from_slice(&[0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 1u8]); // Identity::ContractId
                owner_data.extend_from_slice(&*contract_id.to_vec());
            }
        }
        my_configurables.push((PROXY_OWNER_OFFSET, owner_data));
    
        my_configurables
    }
    ```
    
    /// With the changes
    ```rs
    pub fn build_trade_account_proxy_configurables(
        oracle_id: ContractId,
        owner: Identity,
    ) -> Vec<(u64, Vec<u8>)> {
        TradingAccountProxyConfigurables::default()
            .with_ORACLE_CONTRACT_ID(oracle_id)
            .unwrap()
            .with_INITIAL_OWNER(State::Initialized(owner))
            .unwrap()
            .into()
    }
    ```
    
    The standard implementation is defined as:
    ```sway
    impl SRC12 for Contract {
        #[storage(read, write)]
        fn register_contract(
            child_contract: ContractId,
            configurables: Option<Vec<(u64, Vec<u8>)>>,
        ) -> Result<BytecodeRoot, str> {
    ```
    
    ---------
    
    Co-authored-by: green <xgreenx9999@gmail.com>

--- packages/fuels-core/src/lib.rs
+++ packages/fuels-core/src/lib.rs
@@ -8,12 +8,20 @@ pub use utils::*;
 use crate::types::errors::Result;
 
 #[derive(Debug, Clone, Default, PartialEq)]
+pub struct Configurable {
+    /// The offset (in bytes) within the binary where the data is located.
+    pub offset: u64,
+    /// The data related to the configurable.
+    pub data: Vec<u8>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
 pub struct Configurables {
-    offsets_with_data: Vec<(u64, Vec<u8>)>,
+    pub offsets_with_data: Vec<Configurable>,
 }
 
 impl Configurables {
-    pub fn new(offsets_with_data: Vec<(u64, Vec<u8>)>) -> Self {
+    pub fn new(offsets_with_data: Vec<Configurable>) -> Self {
         Self { offsets_with_data }
     }
 
@@ -21,21 +29,25 @@ impl Configurables {
         let new_offsets_with_data = self
             .offsets_with_data
             .into_iter()
-            .map(|(offset, data)| {
+            .map(|c| {
                 let new_offset = if shift.is_negative() {
-                    offset.checked_sub(shift.unsigned_abs())
+                    c.offset.checked_sub(shift.unsigned_abs())
                 } else {
-                    offset.checked_add(shift.unsigned_abs())
+                    c.offset.checked_add(shift.unsigned_abs())
                 };
 
                 let new_offset = new_offset.ok_or_else(|| {
                     crate::error!(
                         Other,
-                        "Overflow occurred while shifting offset: {offset} + {shift}"
+                        "Overflow occurred while shifting offset: {} + {shift}",
+                        c.offset
                     )
                 })?;
 
-                Ok((new_offset, data.clone()))
+                Ok(Configurable {
+                    offset: new_offset,
+                    data: c.data,
+                })
             })
             .collect::<Result<Vec<_>>>()?;
 
@@ -45,10 +57,16 @@ impl Configurables {
     }
 
     pub fn update_constants_in(&self, binary: &mut [u8]) {
-        for (offset, data) in &self.offsets_with_data {
-            let offset = *offset as usize;
-            binary[offset..offset + data.len()].copy_from_slice(data)
+        for c in &self.offsets_with_data {
+            let offset = c.offset as usize;
+            binary[offset..offset + c.data.len()].copy_from_slice(&c.data)
         }
+    }
+}
+
+impl From<Configurables> for Vec<Configurable> {
+    fn from(config: Configurables) -> Vec<Configurable> {
+        config.offsets_with_data.clone()
     }
 }
 
@@ -58,10 +76,16 @@ mod tests {
 
     #[test]
     fn test_with_shifted_offsets_positive_shift() {
-        let offsets_with_data = vec![(10u64, vec![1, 2, 3])];
+        let offsets_with_data = vec![Configurable {
+            offset: 10u64,
+            data: vec![1, 2, 3],
+        }];
         let configurables = Configurables::new(offsets_with_data.clone());
         let shifted_configurables = configurables.with_shifted_offsets(5).unwrap();
-        let expected_offsets_with_data = vec![(15u64, vec![1, 2, 3])];
+        let expected_offsets_with_data = vec![Configurable {
+            offset: 15u64,
+            data: vec![1, 2, 3],
+        }];
         assert_eq!(
             shifted_configurables.offsets_with_data,
             expected_offsets_with_data
@@ -70,10 +94,16 @@ mod tests {
 
     #[test]
     fn test_with_shifted_offsets_negative_shift() {
-        let offsets_with_data = vec![(10u64, vec![4, 5, 6])];
+        let offsets_with_data = vec![Configurable {
+            offset: 10u64,
+            data: vec![4, 5, 6],
+        }];
         let configurables = Configurables::new(offsets_with_data.clone());
         let shifted_configurables = configurables.with_shifted_offsets(-5).unwrap();
-        let expected_offsets_with_data = vec![(5u64, vec![4, 5, 6])];
+        let expected_offsets_with_data = vec![Configurable {
+            offset: 5u64,
+            data: vec![4, 5, 6],
+        }];
         assert_eq!(
             shifted_configurables.offsets_with_data,
             expected_offsets_with_data
@@ -82,7 +112,10 @@ mod tests {
 
     #[test]
     fn test_with_shifted_offsets_zero_shift() {
-        let offsets_with_data = vec![(20u64, vec![7, 8, 9])];
+        let offsets_with_data = vec![Configurable {
+            offset: 20u64,
+            data: vec![7, 8, 9],
+        }];
         let configurables = Configurables::new(offsets_with_data.clone());
         let shifted_configurables = configurables.with_shifted_offsets(0).unwrap();
         let expected_offsets_with_data = offsets_with_data;
@@ -94,7 +127,10 @@ mod tests {
 
     #[test]
     fn test_with_shifted_offsets_overflow() {
-        let offsets_with_data = vec![(u64::MAX - 1, vec![1, 2, 3])];
+        let offsets_with_data = vec![Configurable {
+            offset: u64::MAX - 1,
+            data: vec![1, 2, 3],
+        }];
         let configurables = Configurables::new(offsets_with_data);
         let result = configurables.with_shifted_offsets(10);
         assert!(result.is_err());
@@ -108,7 +144,10 @@ mod tests {
 
     #[test]
     fn test_with_shifted_offsets_underflow() {
-        let offsets_with_data = vec![(5, vec![4, 5, 6])];
+        let offsets_with_data = vec![Configurable {
+            offset: 5u64,
+            data: vec![4, 5, 6],
+        }];
         let configurables = Configurables::new(offsets_with_data);
         let result = configurables.with_shifted_offsets(-10);
         assert!(result.is_err());
